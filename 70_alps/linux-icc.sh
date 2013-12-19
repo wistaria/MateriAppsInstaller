3dd6daf52dc8b4887e8d9fda3067aa23d8bce5ca
Synge Todo <wistaria@issp.u-tokyo.ac.jp>
2013-12-19 10:12:53 +0900

    Added ALPS installer for linux-icc

---
+++ 70_alps/linux-icc.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
+. $SCRIPT_DIR/../util.sh
+. $SCRIPT_DIR/version.sh
+. $SCRIPT_DIR/../03_boost/version.sh
+set_prefix
+set_build_dir
+
+. $PREFIX_OPT/env.sh
+
+mkdir -p $PREFIX_ALPS/source $PREFIX_ALPS/script
+check cp -p $0 $PREFIX_ALPS/script/compile-$ALPS_VERSION.sh
+check cp -p $HOME/source/alps-$ALPS_VERSION.tar.gz $PREFIX_ALPS/source/
+
+YMDT=`date +%Y%m%d%H%M%S`
+LOG=$PREFIX_ALPS/script/compile-$ALPS_VERSION.$YMDT.log
+rm -f $LOG
+echo "LOG=$LOG"
+
+echo "PREFIX=$PREFIX_ALPS" > $LOG
+echo "ALPS_VERSION=$ALPS_VERSION" >> $LOG
+echo "BOOST_VERSION=$BOOST_VERSION" >> $LOG
+echo "SCRIPT=$PREFIX_ALPS/script/compile-$ALPS_VERSION.sh" >> $LOG
+echo "LOG=$LOG" >> $LOG
+
+(cd $BUILD_DIR && tar zxf $PREFIX_ALPS/source/alps-$ALPS_VERSION.tar.gz)
+
+### OpenMP version
+
+VERSION=$ALPS_VERSION
+echo "[start alps-$VERSION]" >> $LOG 2>&1
+
+rm -rf $BUILD_DIR/alps-build-$VERSION.$YMDT && mkdir -p $BUILD_DIR/alps-build-$VERSION.$YMDT
+cd $BUILD_DIR/alps-build-$VERSION.$YMDT
+echo "[cmake]" >> $LOG 2>&1
+check cmake -DCMAKE_INSTALL_PREFIX=$PREFIX_ALPS/alps-$VERSION \
+  -DCMAKE_C_COMPILER="icc" -DCMAKE_CXX_COMPILER="icpc" -DCMAKE_Fortran_COMPILER="ifort" \
+  -DPYTHON_INTERPRETER=python2.7 \
+  -DHdf5_INCLUDE_DIRS=$PREFIX_OPT/include -DHdf5_LIBRARY_DIRS=$PREFIX_OPT/lib \
+  -DBoost_ROOT_DIR=$PREFIX_OPT/boost_$BOOST_VERSION \
+  -DALPS_ENABLE_OPENMP=ON -DALPS_ENABLE_OPENMP_WORKER=ON \
+  -DALPS_BUILD_FORTRAN=ON \
+  $HOME/build/alps-$ALPS_VERSION >> $LOG 2>&1
+
+echo "[make install]" >> $LOG 2>&1
+check make -j2 install >> $LOG 2>&1
+echo "[ctest]" >> $LOG 2>&1
+ctest >> $LOG 2>&1
+
+cat << EOF >> $PREFIX_ALPS/alpsvars-$VERSION.sh
+. $PREFIX_OPT/env.sh
+. $PREFIX_ALPS/alps-$VERSION/bin/alpsvars.sh
+EOF
+rm -f $PREFIX_ALPS/alpsvars.sh
+ln -s alpsvars-$VERSION.sh $PREFIX_ALPS/alpsvars.sh
+
+### no-OpenMP version
+
+VERSION="noomp-$ALPS_VERSION"
+echo "[start alps-$VERSION]" >> $LOG 2>&1
+
+rm -rf $BUILD_DIR/alps-build-$VERSION.$YMDT && mkdir -p $BUILD_DIR/alps-build-$VERSION.$YMDT
+cd $BUILD_DIR/alps-build-$VERSION.$YMDT
+echo "[cmake]" >> $LOG 2>&1
+check cmake -DCMAKE_INSTALL_PREFIX=$PREFIX_ALPS/alps-$VERSION \
+  -DCMAKE_C_COMPILER="icc" -DCMAKE_CXX_COMPILER="icpc" -DCMAKE_Fortran_COMPILER="ifort" \
+  -DPYTHON_INTERPRETER=python2.7 \
+  -DHdf5_INCLUDE_DIRS=$PREFIX_OPT/include -DHdf5_LIBRARY_DIRS=$PREFIX_OPT/lib \
+  -DBoost_ROOT_DIR=$PREFIX_OPT/boost_$BOOST_VERSION \
+  -DALPS_BUILD_FORTRAN=ON \
+  $HOME/build/alps-$ALPS_VERSION >> $LOG 2>&1
+
+echo "[make install]" >> $LOG 2>&1
+check make -j2 install >> $LOG 2>&1
+echo "[ctest]" >> $LOG 2>&1
+ctest >> $LOG 2>&1
+
+cat << EOF >> $PREFIX_ALPS/alpsvars-$VERSION.sh
+. $PREFIX_OPT/env.sh
+. $PREFIX_ALPS/alps-$VERSION/bin/alpsvars.sh
+EOF
+rm -f $PREFIX_ALPS/alpsvars-noomp.sh
+ln -s alpsvars-$VERSION.sh $PREFIX_ALPS/alpsvars-noomp.sh
