62e419a5d9888fb0b3dab9e94995e19b5e5d36c4
Synge Todo <wistaria@issp.u-tokyo.ac.jp>
2013-12-26 17:59:42 +0900

    Fixed working directory and error detection

--- 10_hdf5/fx10-batch.sh
+++ 10_hdf5/fx10-batch.sh
@@ -51,7 +51,7 @@ check cp $SCRIPT_DIR/fx10-config.cache config.cache
 CC="fccpx -O2 -Xg" check ./configure --cache-file=config.cache --prefix=$PREFIX_BACKEND --target=sparc-linux --host=x86 --enable-threadsafe --with-pthread=yes
 cd src
 check make -j4 H5make_libsettings H5detect
-pjsub --interact $SCRIPT_DIR/fx10-script.sh
+check pjsub --interact $SCRIPT_DIR/fx10-script.sh
 cd ..
 check make -j4
 $SUDO make install
@@ -62,7 +62,6 @@ CC="fccpx -O2 -Xg -KPIC" check ./configure --cache-file=config.cache --prefix=$P
 cd src
 check make -j4 H5make_libsettings H5detect
 check pjsub --interact $SCRIPT_DIR/fx10-script.sh
-cd ..
 check make -j4
 check fccpx -Xg -KPIC -shared -o libhdf5.so H5.o H5checksum.o H5dbg.o H5lib_settings.o H5system.o H5timer.o H5trace.o H5[A-Z]*.o
 $SUDO cp -fp libhdf5.so $PREFIX_BACKEND/lib
