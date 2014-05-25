ab2fb8757d434c051c94d33bd7c29658e3ed03ef
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2014-05-25 10:00:51 +0900

    Updated PREFIX

--- util.sh
+++ util.sh
@@ -43,6 +43,12 @@ set_prefix() {
     PREFIX_ALPS_DEF="/opt/spire/alps"
   fi
 
+  # for kashiwa.issp.u-tokyo.ac.jp
+  if [ -d /home/issp/materiapps ]; then
+    PREFIX_OPT_DEF="/home/issp/materiapps/opt"
+    PREFIX_ALPS_DEF="/home/issp/materiapps/alps"
+  fi
+
   # for maki.issp.u-tokyo.ac.jp
   if [[ ! -z `echo "$HOSTNAME" | egrep "^maki.\.fx10hpc$"` ]]; then
     PREFIX_OPT_DEF="/global/nano/alps"
