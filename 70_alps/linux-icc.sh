994836ce3616cb28269bfce618140a091da06db2
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2014-06-22 16:35:13 +0200

    Updated to build svn

--- 70_alps/linux-icc.sh
+++ 70_alps/linux-icc.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# script alps_source prefix
 
 SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
 . $SCRIPT_DIR/../util.sh
@@ -10,17 +11,28 @@ set_build_dir
 
 . $PREFIX_OPT/env.sh
 
-cd $BUILD_DIR
-if [ -d alps-$ALPS_VERSION ]; then :; else
-  if [ -f $HOME/source/alps-$ALPS_VERSION.tar.gz ]; then
-    check tar zxf $HOME/source/alps-$ALPS_VERSION.tar.gz
-  else
-    check wget -O - http://exa.phys.s.u-tokyo.ac.jp/archive/source/alps-$ALPS_VERSION.tar.gz | tar zxf -
+if [ -n "$1" ]; then
+  ALPS_VERSION="svn"
+  ALPS_SOURCE="$1"
+else
+  ALPS_SOURCE="$BUILD_DIR/alps-$ALPS_VERSION"
+  cd $BUILD_DIR
+  if [ -d alps-$ALPS_VERSION ]; then :; else
+    if [ -f $HOME/source/alps-$ALPS_VERSION.tar.gz ]; then
+      check tar zxf $HOME/source/alps-$ALPS_VERSION.tar.gz
+    else
+      check wget -O - http://exa.phys.s.u-tokyo.ac.jp/archive/source/alps-$ALPS_VERSION.tar.gz | tar zxf -
+    fi
   fi
+  rm -rf $BUILD_DIR/alps-build-$ALPS_VERSION
+fi
+if [ -n "$2" ]; then
+  PREFIX_ALPS="$2"
 fi
 
-rm -rf $BUILD_DIR/alps-build-$ALPS_VERSION && mkdir -p $BUILD_DIR/alps-build-$ALPS_VERSION
+mkdir -p $BUILD_DIR/alps-build-$ALPS_VERSION
 cd $BUILD_DIR/alps-build-$ALPS_VERSION
+rm -rf CMakeCache.txt CPackConfig.cmake CPackSourceConfig.cmake CMakeFiles/2.*
 echo "[cmake]"
 check cmake -DCMAKE_INSTALL_PREFIX=$PREFIX_ALPS/alps-$ALPS_VERSION \
   -DCMAKE_C_COMPILER="icc" -DCMAKE_CXX_COMPILER="icpc" -DCMAKE_Fortran_COMPILER="ifort" \
@@ -28,8 +40,8 @@ check cmake -DCMAKE_INSTALL_PREFIX=$PREFIX_ALPS/alps-$ALPS_VERSION \
   -DHdf5_INCLUDE_DIRS=$PREFIX_OPT/include -DHdf5_LIBRARY_DIRS=$PREFIX_OPT/lib \
   -DBoost_ROOT_DIR=$PREFIX_OPT/boost_$BOOST_VERSION \
   -DALPS_ENABLE_OPENMP=ON -DALPS_ENABLE_OPENMP_WORKER=ON \
-  -DALPS_BUILD_FORTRAN=ON \
-  $BUILD_DIR/alps-$ALPS_VERSION
+  -DALPS_BUILD_FORTRAN=ON -DALPS_BUILD_TESTS=ON -DALPS_BUILD_PYTHON=ON \
+  $ALPS_SOURCE
 
 echo "[make install]"
 check make -j2 install
