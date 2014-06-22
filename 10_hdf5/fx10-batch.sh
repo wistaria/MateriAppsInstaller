4c50c97e818422a8739edca07e43b2f3ebd387bd
Synge Todo <wistaria@issp.u-tokyo.ac.jp>
2014-06-22 17:06:28 +0900

    Added threadsafe option

--- 10_hdf5/fx10-batch.sh
+++ 10_hdf5/fx10-batch.sh
@@ -14,18 +14,6 @@ export LANG C
 # for frontend
 
 cd $BUILD_DIR
-rm -rf szip-$SZIP_VERSION
-if [ -f $HOME/source/szip-$SZIP_VERSION.tar.gz ]; then
-  check tar zxf $HOME/source/szip-$SZIP_VERSION.tar.gz
-else
-  check wget -O - http://www.hdfgroup.org/ftp/lib-external/szip/$SZIP_VERSION/src/szip-$SZIP_VERSION.tar.gz | tar zxf -
-fi
-cd szip-$SZIP_VERSION
-check ./configure --prefix=$PREFIX_FRONTEND
-check make -j4
-$SUDO make install
-
-cd $BUILD_DIR
 rm -rf hdf5-$HDF5_VERSION
 if [ -f $HOME/source/hdf5-$HDF5_VERSION.tar.bz2 ]; then
   check tar jxf $HOME/source/hdf5-$HDF5_VERSION.tar.bz2
@@ -33,7 +21,7 @@ else
   check wget -O - http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.bz2
 fi
 cd hdf5-$HDF5_VERSION
-check ./configure --prefix=$PREFIX_FRONTEND --with-szlib=$PREFIX_FROTEND
+check ./configure --prefix=$PREFIX_FRONTEND --enable-threadsafe --with-pthread=yes
 check make -j4
 $SUDO make install
 
