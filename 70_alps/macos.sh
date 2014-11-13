67ac268ae93def942efa0bda100269866ff3796e
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2014-11-13 19:26:13 +0900

    Added logging of build process

--- 70_alps/macos.sh
+++ 70_alps/macos.sh
@@ -3,10 +3,10 @@
 SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
 . $SCRIPT_DIR/../util.sh
 . $SCRIPT_DIR/version.sh
-. $SCRIPT_DIR/../03_boost/version.sh
 set_prefix
 
 . $PREFIX_TOOL/env.sh
+LOG=$BUILD_DIR/alps-$ALPS_VERSION.log
 
 PREFIX_ALPS="$PREFIX_APPS/alps"
 
@@ -21,26 +21,32 @@ if [ -d alps-$ALPS_VERSION ]; then :; else
   patch -p1 < $SCRIPT_DIR/macosx-accelerate.patch
 fi
 
-rm -rf $BUILD_DIR/alps-build-$ALPS_VERSION
+rm -rf $BUILD_DIR/alps-build-$ALPS_VERSION $LOG
 mkdir -p $BUILD_DIR/alps-build-$ALPS_VERSION
 cd $BUILD_DIR/alps-build-$ALPS_VERSION
-echo "[cmake]"
+start_info | tee -a $LOG
+echo "[cmake]" | tee -a $LOG
 check cmake -DCMAKE_INSTALL_PREFIX=$PREFIX_ALPS/alps-$ALPS_VERSION \
   -DCMAKE_C_COMPILER="/opt/local/bin/gcc" -DCMAKE_CXX_COMPILER="/opt/local/bin/g++" -DCMAKE_Fortran_COMPILER="/opt/local/bin/gfortran" \
   -DPYTHON_INTERPRETER="/opt/local/bin/python2.7" \
-  -DBoost_ROOT_DIR=$PREFIX_TOOL/boost_$BOOST_VERSION \
+  -DBoost_ROOT_DIR=$BOOST_ROOT \
   -DALPS_ENABLE_OPENMP=ON -DALPS_ENABLE_OPENMP_WORKER=ON \
   -DALPS_BUILD_FORTRAN=ON -DALPS_BUILD_TESTS=ON -DALPS_BUILD_PYTHON=ON \
-  $BUILD_DIR/alps-$ALPS_VERSION
+  $BUILD_DIR/alps-$ALPS_VERSION | tee -a $LOG
 
-echo "[make install]"
-check make -j2 install
-echo "[ctest]"
-ctest
+echo "[make]" | tee -a $LOG
+check make -j2 | tee -a $LOG
+echo "[make install]" | tee -a $LOG
+$SUDO_APPS make install | tee -a $LOG
+echo "[ctest]" | tee -a $LOG
+ctest | tee -a $LOG
+finish_info | tee -a $LOG
 
-cat << EOF > $PREFIX_ALPS/alpsvars-$ALPS_VERSION.sh
+cat << EOF > $BUILD_DIR/alpsvars-$ALPS_VERSION.sh
 . $PREFIX_TOOL/env.sh
 . $PREFIX_ALPS/alps-$ALPS_VERSION/bin/alpsvars.sh
 EOF
-rm -f $PREFIX_ALPS/alpsvars.sh
-ln -s alpsvars-$ALPS_VERSION.sh $PREFIX_ALPS/alpsvars.sh
+$SUDO_APPS cp -f $BUILD_DIR/alpsvars-$ALPS_VERSION.sh $PREFIX_ALPS/
+$SUDO_APPS rm -f $PREFIX_ALPS/alpsvars.sh
+$SUDO_APPS ln -s alpsvars-$ALPS_VERSION.sh $PREFIX_ALPS/alpsvars.sh
+$SUDO_APPS cp -f $LOG $PREFIX_ALPS/
