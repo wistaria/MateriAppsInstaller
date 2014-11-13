4c613e1f72a080e58aad54cec1668763254e0bae
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2014-11-13 18:05:26 +0900

    Added support for env.d

--- 00_env/default.sh
+++ 00_env/default.sh
@@ -7,6 +7,16 @@ set_prefix
 cat << EOF > $BUILD_DIR/env.sh
 export PATH=$PREFIX_TOOL/bin:\$PATH
 export LD_LIBRARY_PATH=$PREFIX_TOOL/lib:/usr/lib64:\$LD_LIBRARY_PATH
+for i in $PREFIX_TOOL/env.d/*.sh ; do
+  if [ -r "\$i" ]; then
+    if [ "\${-#*i}" != "\$-" ]; then
+      . "\$i"
+    else
+      . "\$i" >/dev/null 2>&1
+    fi
+  fi
+done
+unset i
 EOF
-$SUDO mkdir -p $PREFIX_TOOL
+$SUDO mkdir -p $PREFIX_TOOL $PREFIX_TOOL/env.d
 $SUDO cp -f $BUILD_DIR/env.sh $PREFIX_TOOL
--- 00_env/fx10.sh
+++ 00_env/fx10.sh
@@ -16,6 +16,16 @@ export CMAKE_PATH=$PREFIX_TOOL/bin/cmake
 export CTEST_PATH=$PREFIX_TOOL/bin/ctest
 export PATH=$PREFIX_TOOL/bin:$PREFIX_TOOL/$(uname -s)-$(uname -m)/bin:/tool/local/gcc/bin:/tool/local/bin:\$PATH
 export LD_LIBRARY_PATH=$PREFIX_TOOL/lib:$PREFIX_TOOL/$(uname -s)-$(uname -m)/lib:/tool/local/gcc/lib64:/tool/local/lib:\$LD_LIBRARY_PATH
+for i in $PREFIX_TOOL/env.d/*.sh ; do
+  if [ -r "\$i" ]; then
+    if [ "\${-#*i}" != "\$-" ]; then
+      . "\$i"
+    else
+      . "\$i" >/dev/null 2>&1
+    fi
+  fi
+done
+unset i
 EOF
-$SUDO mkdir -p $PREFIX_TOOL $PREFIX_TOOL/Linux-x86_64 $PREFIX_TOOL/Linux-s64fx
+$SUDO mkdir -p $PREFIX_TOOL $PREFIX_TOOL/env.d $PREFIX_TOOL/Linux-x86_64 $PREFIX_TOOL/Linux-s64fx
 $SUDO cp -f $BUILD_DIR/env.sh $PREFIX_TOOL
