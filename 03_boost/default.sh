49d84cd33a8a109af893d3d7f2b919cef5dbdc5d
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2014-11-27 19:39:18 +0900

    Updated to 1_55_0.  Merged fx10.sh with default.sh

--- 03_boost/default.sh
+++ 03_boost/default.sh
@@ -7,18 +7,26 @@ set_prefix
 
 . $PREFIX_TOOL/env.sh
 
+if [ -d $PREFIX_TOOL/boost/boost_$BOOST_VERSION-$BOOST_PATCH_VERSION ]; then
+  echo "Error: $PREFIX_TOOL/boost/boost_$BOOST_VERSION-$BOOST_PATCH_VERSION exists"
+  exit 127
+fi
+
 $SUDO_TOOL mkdir -p $PREFIX_TOOL/boost
 cd $PREFIX_TOOL/boost
-$SUDO_TOOL rm -rf boost_$BOOST_VERSION
+$SUDO_TOOL rm -rf boost_$BOOST_VERSION boost_$BOOST_VERSION-$BOOST_PATCH_VERSION
 if [ -f $HOME/source/boost_$BOOST_VERSION.tar.bz2 ]; then
   $SUDO_TOOL tar jxf $HOME/source/boost_$BOOST_VERSION.tar.bz2 --no-same-owner --no-same-permissions
 else
   check wget -O - http://sourceforge.net/projects/boost/files/boost/$BOOST_VERSION/boost_$BOOST_VERSION.tar.bz2/download | $SUDO_TOOL tar jxf - --no-same-owner --no-same-permissions
 fi
+$SUDO_TOOL mv -f boost_$BOOST_VERSION boost_$BOOST_VERSION-$BOOST_PATCH_VERSION
+cd boost_$BOOST_VERSION-$BOOST_PATCH_VERSION
+cat $SCRIPT_DIR/boost_$BOOST_VERSION-$BOOST_PATCH_VERSION.patch | $SUDO_TOOL patch -p1
 
 cat << EOF > $BUILD_DIR/boostvars.sh
-export BOOST_ROOT=$PREFIX_TOOL/boost/boost_$BOOST_VERSION
+export BOOST_ROOT=$PREFIX_TOOL/boost/boost_$BOOST_VERSION-$BOOST_PATCH_VERSION
 EOF
-BOOSTVARS_SH=$PREFIX_TOOL/boost/boostvars-$BOOST_VERSION.sh
+BOOSTVARS_SH=$PREFIX_TOOL/boost/boostvars-$BOOST_VERSION-$BOOST_PATCH_VERSION.sh
 $SUDO_TOOL rm -f $BOOSTVARS_SH
 $SUDO_TOOL cp -f $BUILD_DIR/boostvars.sh $BOOSTVARS_SH
--- 03_boost/link.sh
+++ 03_boost/link.sh
@@ -7,6 +7,6 @@ set_prefix
 
 . $PREFIX_TOOL/env.sh
 
-BOOSTVARS_SH=$PREFIX_TOOL/boost/boostvars-$BOOST_VERSION.sh
+BOOSTVARS_SH=$PREFIX_TOOL/boost/boostvars-$BOOST_VERSION-$BOOST_PATCH_VERSION.sh
 $SUDO_TOOL rm -f $PREFIX_TOOL/env.d/boostvars.sh
 $SUDO_TOOL ln -s $BOOSTVARS_SH $PREFIX_TOOL/env.d/boostvars.sh
