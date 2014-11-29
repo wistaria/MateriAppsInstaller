58e9e4c9ed3c40bfa72bd5d211cfc3925998d01a
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2014-11-29 13:41:16 +0900

    Update 05_hdf5 for default

--- 10_hdf5/default.sh
+++ 10_hdf5/default.sh
@@ -6,6 +6,7 @@ SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
 set_prefix
 
 . $PREFIX_TOOL/env.sh
+PREFIX=$PREFIX_TOOL/hdf5/hdf5-$HDF5_VERSION
 
 cd $BUILD_DIR
 rm -rf hdf5-$HDF5_VERSION
@@ -15,6 +16,14 @@ else
   check wget -O - http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.bz2
 fi
 cd hdf5-$HDF5_VERSION
-check ./configure --prefix=$PREFIX_TOOL --enable-threadsafe --with-pthread=yes
+check ./configure --prefix=$PREFIX --enable-threadsafe --with-pthread=yes
 check make -j4
-$SUDO make install
+$SUDO_TOOL make install
+
+cat << EOF > $BUILD_DIR/hdf5vars.sh
+export PATH=$PREFIX/bin:\$PATH
+export LD_LIBRARY_PATH=$PREFIX/lib:\$LD_LIBRARY_PATH
+EOF
+HDF5VARS_SH=$PREFIX_TOOL/hdf5/hdf5vars-$HDF5_VERSION.sh
+$SUDO_TOOL rm -f $HDF5VARS_SH
+$SUDO_TOOL cp -f $BUILD_DIR/hdf5vars.sh $HDF5VARS_SH
---
+++ 10_hdf5/link.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
+. $SCRIPT_DIR/../util.sh
+. $SCRIPT_DIR/version.sh
+set_prefix
+
+. $PREFIX_TOOL/env.sh
+
+HDF5VARS_SH=$PREFIX_TOOL/hdf5/hdf5vars-$HDF5_VERSION.sh
+$SUDO_TOOL rm -f $PREFIX_TOOL/env.d/hdf5vars.sh
+$SUDO_TOOL ln -s $HDF5VARS_SH $PREFIX_TOOL/env.d/hdf5vars.sh
