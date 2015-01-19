bb08639e46668834d882503cb28aa2e9be3caf57
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2015-01-19 14:51:32 +0900

    Added tapioca

--- 71_xtapp/download.sh
+++ 71_xtapp/download.sh
@@ -59,3 +59,24 @@ if [ -d xtapp-ps_$XTAPP_PS_VERSION ]; then :; else
     check tar zxf xtapp-ps/xtapp-ps_$XTAPP_PS_VERSION.orig.tar.gz
   fi
 fi
+
+cd $BUILD_DIR/xtapp
+if [ -d tapioca_$TAPIOCA_VERSION ]; then :; else
+  if [ -f $SOURCE_DIR/tapioca_$TAPIOCA_VERSION.orig.tar.gz ]; then
+    check tar zxf $SOURCE_DIR/tapioca_$TAPIOCA_VERSION.orig.tar.gz
+  else
+    check wget $MALIVE_REPOSITORY/main/t/tapioca/tapioca_$TAPIOCA_VERSION.orig.tar.gz
+    check tar zxf tapioca/tapioca_$TAPIOCA_VERSION.orig.tar.gz
+  fi
+  cd tapioca-$TAPIOCA_VERSION
+  if [ -f $SOURCE_DIR/tapioca_$TAPIOCA_VERSION-$TAPIOCA_PATCH_VERSION.debian.tar.gz ]; then
+    tar zxf $SOURCE_DIR/tapioca_$TAPIOCA_VERSION-$TAPIOCA_PATCH_VERSION.debian.tar.gz
+  else
+    check wget $MALIVE_REPOSITORY/main/t/tapioca/tapioca_$TAPIOCA_VERSION-$TAPIOCA_PATCH_VERSION.debian.tar.gz
+    check tar zxf tapioca_$TAPIOCA_VERSION-$TAPIOCA_PATCH_VERSION.debian.tar.gz
+  fi
+  PATCHES="makefile-1.patch"
+  for p in $PATCHES; do
+    patch -p1 < debian/patches/$p
+  done
+fi
--- 71_xtapp/macos.sh
+++ 71_xtapp/macos.sh
@@ -20,25 +20,48 @@ rm -rf $LOG
 cd $BUILD_DIR/xtapp
 patch -p1 < $SCRIPT_DIR/xtapp-macos.patch
 start_info | tee -a $LOG
-cd $BUILD_DIR/xtapp/src
-echo "[make]" | tee -a $LOG
+echo "[make xtapp]" | tee -a $LOG
 check make | tee -a $LOG
-echo "[make install]" | tee -a $LOG
+
+echo "[make xtapp-util]" | tee -a $LOG
+cd $BUILD_DIR/xtapp/xtapp-util
+check make | tee -a $LOG
+
+echo "[make tapioca]" | tee -a $LOG
+cd $BUILD_DIR/xtapp/tapioca-$TAPIOCA_VERSION
+check make | tee -a $LOG
+
+echo "[make install xtapp]" | tee -a $LOG
+cd $BUILD_DIR/xtapp/src
 $SUDO_APPS make install PREFIX=$PREFIX | tee -a $LOG
 cd $BUILD_DIR/xtapp
-$SUDO_APPS cp -frp sample debian/check debian/opendx $PREFIX
-cd $BUILD_DIR/xtapp/xtapp-util
-echo "[make xtapp-util]" | tee -a $LOG
-check make | tee -a $LOG
+$SUDO_APPS cp -rp sample debian/check debian/opendx $PREFIX
+
 echo "[make install xtapp-util]" | tee -a $LOG
+cd $BUILD_DIR/xtapp/xtapp-util
 $SUDO_APPS make install PREFIX=$PREFIX | tee -a $LOG
-$SUDO_APPS cp -fp conv-tapp3/ppot2x vbpef2gp-lsda/vbpef2gp-lsda $PREFIX/bin
-cd $BUILD_DIR/xtapp/xtapp-ps_$XTAPP_PS_VERSION
+$SUDO_APPS cp -p conv-tapp3/ppot2x vbpef2gp-lsda/vbpef2gp-lsda $PREFIX/bin
+
 echo "[make install xtapp-ps]" | tee -a $LOG
+cd $BUILD_DIR/xtapp/xtapp-ps_$XTAPP_PS_VERSION
 $SUDO_APPS mkdir -p $PREFIX/pseudo-potential/PBE $PREFIX/pseudo-potential/PBE-nc $PREFIX/pseudo-potential/PBE-CAPZ
-$SUDO_APPS cp -frp xTAPP-PS-PBE/* $PREFIX/pseudo-potential/PBE
-$SUDO_APPS cp -frp xTAPP-PS-PBE-nc/* $PREFIX/pseudo-potential/PBE-nc
-$SUDO_APPS cp -frp xTAPP-PS-CAPZ/* $PREFIX/pseudo-potential/PBE-CAPZ
+$SUDO_APPS cp -rp xTAPP-PS-PBE/* $PREFIX/pseudo-potential/PBE
+$SUDO_APPS cp -rp xTAPP-PS-PBE-nc/* $PREFIX/pseudo-potential/PBE-nc
+$SUDO_APPS cp -rp xTAPP-PS-CAPZ/* $PREFIX/pseudo-potential/PBE-CAPZ
+
+echo "[make install tapioca]" | tee -a $LOG
+cd $BUILD_DIR/xtapp/tapioca-$TAPIOCA_VERSION
+$SUDO_APPS make install PREFIX=$PREFIX | tee -a $LOG
+$SUDO_APPS mkdir -p $PREFIX/tapica
+$SUDO_APPS cp -rp sample $PREFIX/tapioca
+$SUDO_APPS cp -p src/defaults.*ml src/kpath.xml src/spacegroup.db $PREFIX/libexec
+cat << EOF > $BUILD_DIR/tapioca
+#!/bin/sh
+$PREFIX/libexec "$@"
+exit $?
+EOF
+chmod +x $BUILD_DIR/tapioca 
+$SUDO_APPS cp -p $BUILD_DIR/tapioca $PREFIX/bin
 finish_info | tee -a $LOG
 
 cat << EOF > $BUILD_DIR/xtappvars.sh
