d34e8fc66968c21b2834161ac8405310b2509602
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2015-01-20 17:31:29 +0900

    Fixed path to Qt4

--- 80_tapioca/intel-mkl.sh
+++ 80_tapioca/intel-mkl.sh
@@ -6,6 +6,7 @@ SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
 set_prefix
 
 . $PREFIX_TOOL/env.sh
+. $PREFIX_APPS/xtapp/xtappvars.sh
 LOG=$BUILD_DIR/tapioca-$TAPIOCA_VERSION-$TAPIOCA_PATCH_VERSION.log
 
 PREFIX="$PREFIX_APPS/tapioca/tapioca-$TAPIOCA_VERSION-$TAPIOCA_PATCH_VERSION"
@@ -18,27 +19,33 @@ fi
 sh $SCRIPT_DIR/download.sh
 rm -rf $LOG
 cd $BUILD_DIR/tapioca-$TAPIOCA_VERSION
-patch -p1 < $SCRIPT_DIR/tapioca-intel-mkl.patch
+if [ -n $QT4_ROOT ]; then
+  patch -p1 < $SCRIPT_DIR/tapioca-intel-mkl-qt4.patch
+else
+  patch -p1 < $SCRIPT_DIR/tapioca-intel-mkl.patch
+fi
 start_info | tee -a $LOG
 echo "[make tapioca]" | tee -a $LOG
 check make | tee -a $LOG
 echo "[make install tapioca]" | tee -a $LOG
 $SUDO_APPS make install PREFIX=$PREFIX | tee -a $LOG
-$SUDO_APPS cp -rp sample $PREFIX
-$SUDO_APPS cp -p src/defaults.*ml src/kpath.xml src/spacegroup.db $PREFIX/libexec
+$SUDO_APPS cp -r sample $PREFIX
+$SUDO_APPS cp -f src/defaults.*ml src/kpath.xml src/spacegroup.db $PREFIX/libexec
 cat << EOF > $BUILD_DIR/tapioca
 #!/bin/sh
 $PREFIX/libexec/tapioca "$@"
 exit $?
 EOF
-chmod +x $BUILD_DIR/tapioca 
-$SUDO_APPS cp -p $BUILD_DIR/tapioca $PREFIX/bin
+$SUDO_APPS mkdir -p $PREFIX/bin
+$SUDO_APPS cp -f $BUILD_DIR/tapioca $PREFIX/bin
+$SUDO_APPS chmod +x $PREFIX/bin/tapioca
 finish_info | tee -a $LOG
 
 cat << EOF > $BUILD_DIR/tapiocavars.sh
 . $PREFIX_TOOL/env.sh
+export XTAPP_ROOT=$XTAPP_ROOT
 export TAPIOCA_ROOT=$PREFIX
-export PATH=\$TAPIOCA_ROOT/bin:\$PATH
+export PATH=\$XTAPP_ROOT/bin:\$TAPIOCA_ROOT/bin:\$PATH
 EOF
 TAPIOCAVARS_SH=$PREFIX_APPS/tapioca/tapiocavars-$TAPIOCA_VERSION-$TAPIOCA_PATCH_VERSION.sh
 $SUDO_APPS rm -f $TAPIOCAVARS_SH
