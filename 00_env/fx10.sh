0b25a5bd857015a1e77c3d597d586b3689c4ecaf
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2015-04-08 14:59:55 +0900

    Added check_maversion script

--- 00_env/fx10.sh
+++ 00_env/fx10.sh
@@ -12,6 +12,8 @@ if [ -f /home/system/Env_base_$ENV_VERSION ]; then
 elif [ -f /work/system/Env_base_$ENV_VERSION ]; then
   . /work/system/Env_base_$ENV_VERSION
 fi
+export MA_ROOT_TOOL=$PREFIX_TOOL
+export MA_ROOT_APPS=$PREFIX_APPS
 export PATH=$PREFIX_TOOL/bin:/opt/local/gcc/bin:/opt/local/bin:\$PATH
 export LD_LIBRARY_PATH=$PREFIX_TOOL/lib:/opt/local/gcc/lib64:/opt/local/lib:\$LD_LIBRARY_PATH
 for i in $PREFIX_TOOL/env.d/*.sh ; do
@@ -25,5 +27,7 @@ for i in $PREFIX_TOOL/env.d/*.sh ; do
 done
 unset i
 EOF
-$SUDO_TOOL mkdir -p $PREFIX_TOOL $PREFIX_TOOL/env.d
+$SUDO_TOOL mkdir -p $PREFIX_TOOL $PREFIX_TOOL/env.d $PREFIX_TOOL/bin
 $SUDO_TOOL cp -f $BUILD_DIR/env.sh $PREFIX_TOOL
+$SUDO_TOOL cp -f $SCRIPT_DIR/check_maversion $PREFIX_TOOL/bin
+$SUDO_TOOL chmod +x $PREFIX_TOOL/bin/check_maversion
