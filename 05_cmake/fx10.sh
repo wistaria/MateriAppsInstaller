ec2d81cdf94e40fa016013b9b7cdbda3eb0d6789
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2015-04-08 19:14:53 +0900

    Updated for 3.2.1

--- 05_cmake/fx10.sh
+++ 05_cmake/fx10.sh
@@ -35,12 +35,9 @@ cd $BUILD_DIR
 check rm -rf cmake-$CMAKE_VERSION-Linux-s64fx
 check mkdir -p cmake-$CMAKE_VERSION-Linux-s64fx
 check cd cmake-$CMAKE_VERSION-Linux-s64fx
-CC="fccpx -Xg" CFLAGS="-O2 -DcmIML_ABI_NO_ERROR_LONG_LONG" CXX="FCCpx -Xg" CXXFLAGS="-O2 -DcmIML_ABI_NO_ERROR_LONG_LONG" $PREFIX_FRONTEND/bin/cmake -DCMAKE_INSTALL_PREFIX=$PREFIX_BACKEND -DUPDATE_TYPE=svn -DHAVE_FCHDIR=1 -DHAVE_FCHMOD=1 -DHAVE_FCHOWN=1 -DHAVE_FCNTL=1 -DHAVE_UTIMES=1 check $BUILD_DIR/cmake-$CMAKE_VERSION
-check make ctest cpack ccmake cmake
-check $PREFIX_FRONTEND/bin/ctest --help-full Docs/ctest.txt --help-full Docs/ctest.html --help-full Docs/ctest.1 --help-full Docs/ctest.docbook
-check $PREFIX_FRONTEND/bin/cpack --help-full Docs/cpack.txt --help-full Docs/cpack.html --help-full Docs/cpack.1 --help-full Docs/cpack.docbook
-check $PREFIX_FRONTEND/bin/ccmake --help-full Docs/ccmake.txt --help-full Docs/ccmake.html --help-full Docs/ccmake.1 --help-full Docs/ccmake.docbook
-check $PREFIX_FRONTEND/bin/cmake --copyright Docs/Copyright.txt --help-full Docs/cmake.txt --help-full Docs/cmake.html --help-full Docs/cmake.1 --help-full Docs/cmake.docbook --help-policies Docs/cmake-policies.txt --help-policies Docs/cmake-policies.html --help-policies Docs/cmakepolicies.1 --help-properties Docs/cmake-properties.txt --help-properties Docs/cmake-properties.html --help-properties Docs/cmakeprops.1 --help-variables Docs/cmake-variables.txt --help-variables Docs/cmake-variables.html --help-variables Docs/cmakevars.1 --help-modules Docs/cmake-modules.txt --help-modules Docs/cmake-modules.html --help-modules Docs/cmakemodules.1 --help-commands Docs/cmake-commands.txt --help-commands Docs/cmake-commands.html --help-commands Docs/cmakecommands.1 --help-compatcommands Docs/cmake-compatcommands.txt --help-compatcommands Docs/cmake-compatcommands.html --help-compatcommands Docs/cmakecompat.1
+CC="fccpx -Xg" CFLAGS="-O2 -DcmIML_ABI_NO_ERROR_LONG_LONG" CXX="FCCpx -Xg" CXXFLAGS="-O2 -DcmIML_ABI_NO_ERROR_LONG_LONG" check $PREFIX_FRONTEND/bin/cmake -DCMAKE_INSTALL_PREFIX=$PREFIX_BACKEND -DUPDATE_TYPE=svn -DHAVE_FCHDIR=1 -DHAVE_FCHMOD=1 -DHAVE_FCHOWN=1 -DHAVE_FCNTL=1 -DHAVE_UTIMES=1 -DHAVE_POSIX_STRERROR_R=1 $BUILD_DIR/cmake-$CMAKE_VERSION
+CC="fccpx -Xg" CFLAGS="-O2 -DcmIML_ABI_NO_ERROR_LONG_LONG" CXX="FCCpx -Xg" CXXFLAGS="-O2 -DcmIML_ABI_NO_ERROR_LONG_LONG" check $PREFIX_FRONTEND/bin/cmake -DCMAKE_INSTALL_PREFIX=$PREFIX_BACKEND -DUPDATE_TYPE=svn -DHAVE_FCHDIR=1 -DHAVE_FCHMOD=1 -DHAVE_FCHOWN=1 -DHAVE_FCNTL=1 -DHAVE_UTIMES=1 -DHAVE_POSIX_STRERROR_R=1 $BUILD_DIR/cmake-$CMAKE_VERSION # run twice!
+check make -j4
 check $PREFIX_FRONTEND/bin/cmake -P cmake_install.cmake
 
 cat << EOF > $BUILD_DIR/cmake
@@ -58,6 +55,7 @@ $SUDO_TOOL chmod +x $PREFIX/bin/cmake $PREFIX/bin/ctest
 
 cat << EOF > $BUILD_DIR/cmakevars.sh
 export CMAKE_ROOT=$PREFIX
+export CMAKE_VERSION=$CMAKE_VERSION
 export PATH=\$CMAKE_ROOT/bin:\$PATH
 export CMAKE_PATH=\$CMAKE_ROOT/bin/cmake
 export CTEST_PATH=\$CMAKE_ROOT/bin/ctest
