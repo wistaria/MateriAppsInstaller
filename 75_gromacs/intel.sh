a61b93f7c4516508aad54b100907f65341063847
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2015-06-03 13:39:15 +0900

    Supported FFTW installed by MateriAppsInstaller

--- 75_gromacs/intel.sh
+++ 75_gromacs/intel.sh
@@ -21,7 +21,11 @@ mkdir -p $BUILD_DIR/gromacs-$GROMACS_VERSION-build
 cd $BUILD_DIR/gromacs-$GROMACS_VERSION-build
 start_info | tee -a $LOG
 echo "[make]" | tee -a $LOG
-check cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DGMX_MPI=on $BUILD_DIR/gromacs-$GROMACS_VERSION | tee -a $LOG
+if [ -n $FFTW_ROOT ]; then
+  check cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DGMX_MPI=on -DFFTWF_INCLUDE_DIR="$FFTW_ROOT/include" -DFFTWF_LIBRARY="$FFTW_ROOT/lib/libfftw3f.so" $BUILD_DIR/gromacs-$GROMACS_VERSION | tee -a $LOG
+else
+  check cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DGMX_MPI=on $BUILD_DIR/gromacs-$GROMACS_VERSION | tee -a $LOG
+fi
 check make | tee -a $LOG
 echo "[make install]" | tee -a $LOG
 $SUDO_APPS make install | tee -a $LOG
@@ -30,6 +34,8 @@ finish_info | tee -a $LOG
 cat << EOF > $BUILD_DIR/gromacsvars.sh
 . $PREFIX_TOOL/env.sh
 export GROMACS_ROOT=$PREFIX
+export GROMACS_VERSION=$GROMACS_VERSION
+export GROMACS_PATCH_VERSION=$GROMACS_PATCH_VERSION
 export PATH=\$GROMACS_ROOT/bin:\$PATH
 EOF
 GROMACSVARS_SH=$PREFIX_APPS/gromacs/gromacsvars-$GROMACS_VERSION-$GROMACS_PATCH_VERSION.sh
