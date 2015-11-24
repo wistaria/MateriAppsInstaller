f29db5816409c2419f9e08f09e0a022a51e06799
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2015-11-24 18:59:59 +0900

    Fixed file mode of install-sh, changed MPI C++ compiler name

--- 77_dsqss/setup.sh
+++ 77_dsqss/setup.sh
@@ -12,11 +12,16 @@ if [ -d dsqss-$DSQSS_VERSION ]; then :; else
   if [ -f $SOURCE_DIR/dsqss-v$DSQSS_VERSION.zip ]; then
     check unzip $SOURCE_DIR/dsqss-v$DSQSS_VERSION.zip
   else
-    check wget https://github.com/qmc/dsqss/archive/v$DSQSS_VERSION.zip -O dsqss-v$DSQSS_VERSION.zip
-    check unzip dsqss-v$DSQSS_VERSION.zip
+    check wget https://github.com/qmc/dsqss/archive/v$DSQSS_VERSION.zip -O dsqss-$DSQSS_VERSION.zip
+    check unzip dsqss-$DSQSS_VERSION.zip
   fi
   if [ $DSQSS_VERSION != $DSQSS_VERSION_ORIG ]; then
     mv dsqss-$DSQSS_VERSION_ORIG dsqss-$DSQSS_VERSION
   fi
   cd dsqss-$DSQSS_VERSION
+  PATCHES="runConfigure.patch"
+  for p in $PATCHES; do
+    patch -p1 < $SCRIPT_DIR/$p
+  done
+  chmod +x dsqss/dsqss-1.1.17/install-sh
 fi
