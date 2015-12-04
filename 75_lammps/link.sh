c3f2aa054c2ba75bdf1ac46f8e49979223d53b90
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2015-12-04 11:14:46 +0900

    Added LAMMPS

---
+++ 75_lammps/link.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
+. $SCRIPT_DIR/../util.sh
+. $SCRIPT_DIR/version.sh
+set_prefix
+
+. $PREFIX_TOOL/env.sh
+
+LAMMPSVARS_SH=$PREFIX_APPS/lammps/lammpsvars-$LAMMPS_VERSION-$LAMMPS_MA_REVISION.sh
+$SUDO_APPS rm -f $PREFIX_APPS/lammps/lammpsvars.sh
+$SUDO_APPS ln -s $LAMMPSVARS_SH $PREFIX_APPS/lammps/lammpsvars.sh
---
+++ 75_lammps/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
+. $SCRIPT_DIR/../util.sh
+. $SCRIPT_DIR/version.sh
+set_prefix
+
+cd $BUILD_DIR
+if [ -d lammps-$LAMMPS_VERSION ]; then :; else
+  if [ -f $SOURCE_DIR/lammps-$LAMMPS_VERSION.tar.gz ]; then
+    check tar zxf $SOURCE_DIR/lammps_$LAMMPS_VERSION.tar.gz
+  else
+    check wget $WGET_OPTION http://lammps.sandia.gov/tars/lammps-$LAMMPS_VERSION.tar.gz
+    check tar zxf lammps-$LAMMPS_VERSION.tar.gz
+  fi
+fi
