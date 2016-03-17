c67f964e79f07b12ebf6d4fe5a3d1cf55a2aa933
Synge Todo <wistaria@issp.u-tokyo.ac.jp>
2016-03-17 10:22:49 +0900

    Fixed setup script

--- 30_cmake/setup.sh
+++ 30_cmake/setup.sh
@@ -13,7 +13,7 @@ if [ -d cmake-$CMAKE_VERSION ]; then :; else
     check tar zxf $HOME/source/cmake-$CMAKE_VERSION.tar.gz
   else
     check wget http://www.cmake.org/files/v$CMAKE_VERSION_MAJOR/cmake-$CMAKE_VERSION.tar.gz
-    check tar zxf - cmake-$CMAKE_VERSION.tar.gz
+    check tar zxf cmake-$CMAKE_VERSION.tar.gz
   fi
   cd $BUILD_DIR/cmake-$CMAKE_VERSION
   check patch -p1 < $SCRIPT_DIR/cmake-$CMAKE_VERSION.patch
