f218d0cb17297039f25417539caaad2f2ee818f5
Yuichi Motoyama <yomichi@tsg.jp>
2016-04-06 13:58:05 +0900

    fixed filename

--- 78_HPhi/setup.sh
+++ 78_HPhi/setup.sh
@@ -8,7 +8,7 @@ set_prefix
 cd $BUILD_DIR
 if [ -d HPhi-$HPHI_VERSION ]; then :; else
   if [ -f $SOURCE_DIR/HPhi-release-$HPHI_VERSION.tar.gz ]; then
-    check tar zxf $SOURCE_DIR/HPhi_$HPHI_VERSION.tar.gz
+    check tar zxf $SOURCE_DIR/HPhi-release-$HPHI_VERSION.tar.gz
   else
     if [ -f HPhi-release-$HPhi_VERSION.tar.gz ]; then :; else
       check wget https://github.com/QLMS/HPhi/releases/download/v${HPHI_VERSION}/HPhi-release-${HPHI_VERSION}.tar.gz -O HPhi-release-${HPHI_VERSION}.tar.gz
