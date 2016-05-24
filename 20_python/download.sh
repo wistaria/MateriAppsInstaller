f5300c9a181c4e8d26ba6d912442ef705f718b98
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2016-05-24 18:01:51 +0900

    Added wheel

--- 20_python/download.sh
+++ 20_python/download.sh
@@ -6,8 +6,8 @@ set_prefix
 . $PREFIX_TOOL/env.sh
 . $SCRIPT_DIR/version.sh
 
-wget $WGET_OPTION http://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz
-wget $WGET_OPTION https://bootstrap.pypa.io/get-pip.py
-wget $WGET_OPTION http://pypi.python.org/packages/source/s/scipy/scipy-$SCIPY_VERSION.tar.gz
-wget $WGET_OPTION http://pypi.python.org/packages/source/n/numpy/numpy-$NUMPY_VERSION.tar.gz
-pip --no-cache-dir download pip matplotlib ipython mock
+wget $WGET_OPTION -O Python-$PYTHON_VERSION.tgz http://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz
+wget $WGET_OPTION -O get-pip.py https://bootstrap.pypa.io/get-pip.py
+wget $WGET_OPTION -O scipy-$SCIPY_VERSION.tar.gz http://pypi.python.org/packages/source/s/scipy/scipy-$SCIPY_VERSION.tar.gz
+wget $WGET_OPTION -O numpy-$NUMPY_VERSION.tar.gz http://pypi.python.org/packages/source/n/numpy/numpy-$NUMPY_VERSION.tar.gz
+pip --no-cache-dir download pip wheel matplotlib ipython mock
