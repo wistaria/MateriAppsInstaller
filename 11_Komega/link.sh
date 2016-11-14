e858e93349c933c8169e6f76b5ccba4b838ee252
Yuichi Motoyama <yomichi@tsg.jp>
2016-11-14 14:34:25 +0900

    fix prefix

--- 11_Komega/link.sh
+++ 11_Komega/link.sh
@@ -7,7 +7,7 @@ set_prefix
 
 . $PREFIX_TOOL/env.sh
 
-KOMEGAVARS_SH=$PREFIX_APPS/Komega/Komegavars-$KOMEGA_VERSION-$KOMEGA_PATCH_VERSION.sh
-$SUDO_APPS rm -f $PREFIX_APPS/Komega/Komegavars.sh
-$SUDO_APPS ln -s $KOMEGAVARS_SH $PREFIX_APPS/Komega/Komegavars.sh
+KOMEGAVARS_SH=$PREFIX_TOOL/Komega/Komegavars-$KOMEGA_VERSION-$KOMEGA_PATCH_VERSION.sh
+$SUDO_APPS rm -f $PREFIX_TOOL/Komega/Komegavars.sh
+$SUDO_APPS ln -s $KOMEGAVARS_SH $PREFIX_TOOL/Komega/Komegavars.sh
 
