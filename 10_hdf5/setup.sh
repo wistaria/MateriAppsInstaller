796ab47c55f5b7a6277bfa8765a4818209e18bf4
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2016-11-15 16:12:36 +0900

    Updated download links

--- 10_hdf5/setup.sh
+++ 10_hdf5/setup.sh
@@ -11,7 +11,7 @@ if [ -d hdf5-$HDF5_VERSION ]; then :; else
   if [ -f $SOURCE_DIR/hdf5-$HDF5_VERSION.tar.bz2 ]; then
     check tar jxf $SOURCE_DIR/hdf5-$HDF5_VERSION.tar.bz2
   else
-    check wget http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.bz2
+    check wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.bz2
     tar jxf hdf5-$HDF5_VERSION.tar.bz2
   fi
 fi
