3dcae8f81c6c35a11b4d929de935e3b121e45e21
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2016-12-04 12:04:06 +0900

    Updated env to support cxx1y

--- 00_env/default.sh
+++ 00_env/default.sh
@@ -5,10 +5,11 @@ SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
 . $SCRIPT_DIR/version.sh
 set_prefix
 
-cat << EOF > $BUILD_DIR/env.sh
+cat << EOF > $BUILD_DIR/env-cxx03.sh
 # env $(basename $0 .sh) $ENV_VERSION $ENV_MA_REVISION $(date +%Y%m%d-%H%M%S)
 export MA_ROOT_TOOL=$PREFIX_TOOL
 export MA_ROOT_APPS=$PREFIX_APPS
+export MA_CXX_STANDARD=cxx03
 export PATH=$PREFIX_TOOL/bin:\$PATH
 export LD_LIBRARY_PATH=$PREFIX_TOOL/lib:\$LD_LIBRARY_PATH
 for i in $PREFIX_TOOL/env.d/*.sh ; do
@@ -22,8 +23,30 @@ for i in $PREFIX_TOOL/env.d/*.sh ; do
 done
 unset i
 EOF
-$SUDO_TOOL mkdir -p $PREFIX_TOOL $PREFIX_TOOL/env.d $PREFIX_TOOL/bin
-$SUDO_TOOL cp -f $BUILD_DIR/env.sh $PREFIX_TOOL
-rm -f $BUILD_DIR/env.sh
+
+cat << EOF > $BUILD_DIR/env-cxx1y.sh
+# env $(basename $0 .sh) $ENV_VERSION $ENV_MA_REVISION $(date +%Y%m%d-%H%M%S)
+export MA_ROOT_TOOL=$PREFIX_TOOL
+export MA_ROOT_APPS=$PREFIX_APPS
+export MA_CXX_STANDARD=cxx1y
+export PATH=$PREFIX_TOOL/bin:\$PATH
+export LD_LIBRARY_PATH=$PREFIX_TOOL/lib:\$LD_LIBRARY_PATH
+for i in $PREFIX_TOOL/env.d/*.sh ; do
+  if [ -r "\$i" ]; then
+    if [ "\${-#*i}" != "\$-" ]; then
+      . "\$i"
+    else
+      . "\$i" >/dev/null 2>&1
+    fi
+  fi
+done
+unset i
+EOF
+
+$SUDO_TOOL mkdir -p $PREFIX_TOOL/env.d $PREFIX_TOOL/bin $PREFIX_TOOL/lib
+$SUDO_TOOL cp -f $BUILD_DIR/env-cxx03.sh $BUILD_DIR/env-cxx1y.sh $PREFIX_TOOL
+$SUDO_TOOL rm -f $PREFIX_TOOL/env.sh
+$SUDO_TOOL ln -s env-cxx03.sh $PREFIX_TOOL/env.sh
+rm -f $BUILD_DIR/env-cxx03.sh $BUILD_DIR/env-cxx1y.sh
 $SUDO_TOOL cp -f $SCRIPT_DIR/check_maversion $PREFIX_TOOL/bin
 $SUDO_TOOL chmod +x $PREFIX_TOOL/bin/check_maversion
