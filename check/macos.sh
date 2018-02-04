dfc5059664123169f4afdda77c9f3a98b26db635
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2018-02-04 23:35:22 +0900

    Add check script for macos

---
+++ check/macos.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
+MAINSTALLER_CONFIG=$HOME/.mainstaller-check
+PREFIX=$HOME/materiapps-check
+BUILD_DIR=$PREFIX/build
+
+rm -f $MAINSTALLER_CONFIG $PREFIX $BUILD_DIR
+mkdir -p $PREFIX $BUILD_DIR
+
+cat << EOF > $MAINSTALLER_CONFIG
+PREFIX=$PREFIX
+BUILD_DIR=$BUILD_DIR
+EOF
+
+###
+
+export MAINSTALLER_CONFIG
+sh $SCRIPT_DIR/../00_env/default.sh
+sh $SCRIPT_DIR/../25_boost/macos.sh && sh $SCRIPT_DIR/../25_boost/link.sh
+sh $SCRIPT_DIR/../70_alps/macos.sh && sh $SCRIPT_DIR/../70_alps/link.sh
