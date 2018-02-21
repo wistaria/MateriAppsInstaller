ab6f2951aeb481a68050ede91bab962145ab6357
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2018-02-21 15:56:02 +0900

    Update check/sekirei.sh

--- check/sekirei.sh
+++ check/sekirei.sh
@@ -5,7 +5,7 @@ MAINSTALLER_CONFIG=$HOME/.mainstaller-check
 PREFIX=$HOME/materiapps-check
 BUILD_DIR=$PREFIX/build
 
-# rm -rf $MAINSTALLER_CONFIG $PREFIX $BUILD_DIR
+rm -rf $MAINSTALLER_CONFIG $PREFIX $BUILD_DIR
 mkdir -p $PREFIX $BUILD_DIR
 
 cat << EOF > $MAINSTALLER_CONFIG
@@ -23,14 +23,14 @@ eval \`/usr/bin/modulecmd bash remove intel gnu\`
 eval \`/usr/bin/modulecmd bash load intel/18.0.1.163 gnu/7.2.0\`
 eval \`/usr/bin/modulecmd bash list\`
 EOF
-# sh $SCRIPT_DIR/../00_wget/default.sh && sh $SCRIPT_DIR/../00_wget/link.sh
-# sh $SCRIPT_DIR/../06_fftw/intel.sh && sh $SCRIPT_DIR/../06_fftw/link.sh
-# sh $SCRIPT_DIR/../10_hdf5/default.sh && sh $SCRIPT_DIR/../10_hdf5/link.sh
-# sh $SCRIPT_DIR/../11_eigen3/default.sh && sh $SCRIPT_DIR/../11_eigen3/link.sh
-# sh $SCRIPT_DIR/../20_python/intel-mkl.sh && sh $SCRIPT_DIR/../20_python/link.sh
+sh $SCRIPT_DIR/../00_wget/default.sh && sh $SCRIPT_DIR/../00_wget/link.sh
+sh $SCRIPT_DIR/../06_fftw/intel.sh && sh $SCRIPT_DIR/../06_fftw/link.sh
+sh $SCRIPT_DIR/../10_hdf5/default.sh && sh $SCRIPT_DIR/../10_hdf5/link.sh
+sh $SCRIPT_DIR/../11_eigen3/default.sh && sh $SCRIPT_DIR/../11_eigen3/link.sh
+sh $SCRIPT_DIR/../20_python/intel-mkl.sh && sh $SCRIPT_DIR/../20_python/link.sh
 sh $SCRIPT_DIR/../25_boost/intel.sh && sh $SCRIPT_DIR/../25_boost/link.sh
-# sh $SCRIPT_DIR/../30_cmake/default.sh && sh $SCRIPT_DIR/../30_cmake/link.sh
-# sh $SCRIPT_DIR/../35_git/default.sh && sh $SCRIPT_DIR/../35_git/link.sh
+sh $SCRIPT_DIR/../30_cmake/default.sh && sh $SCRIPT_DIR/../30_cmake/link.sh
+sh $SCRIPT_DIR/../35_git/default.sh && sh $SCRIPT_DIR/../35_git/link.sh
 sh $SCRIPT_DIR/../70_alps/intel-mkl.sh && sh $SCRIPT_DIR/../70_alps/link.sh
 
 ###
