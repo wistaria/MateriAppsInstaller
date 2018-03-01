cf50561de42010a72d2e539055f2ee2f28c834ba
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2018-03-01 18:44:51 +0900

    lammps 20170811-1

--- 75_lammps/intel.sh
+++ 75_lammps/intel.sh
@@ -16,24 +16,85 @@ fi
 
 sh $SCRIPT_DIR/setup.sh
 rm -rf $LOG
-cd $BUILD_DIR/lammps-$LAMMPS_VERSION/src
-cp MAKE/OPTIONS/Makefile.icc_serial MAKE
-cd STUBS
-make clean
-make CC=icc
-cd $BUILD_DIR/lammps-$LAMMPS_VERSION/src
 start_info | tee -a $LOG
-echo "[make]" | tee -a $LOG
-check make mpi | tee -a $LOG
-check make icc_serial | tee -a $LOG
-check make mpi mode=lib | tee -a $LOG
-check make mpi mode=shlib | tee -a $LOG
-check make icc_serial mode=lib | tee -a $LOG
-check make icc_serial mode=shlib | tee -a $LOG
+echo "[enable packages]" | tee -a $LOG
+cd $BUILD_DIR/lammps-$LAMMPS_VERSION/src
+check make yes-all | tee -a $LOG
+check make no-gpu | tee -a $LOG
+check make no-kim | tee -a $LOG
+check make no-kokkos | tee -a $LOG
+check make no-voronoi | tee -a $LOG
+check make no-user-molfile | tee -a $LOG
+check make no-user-quip | tee -a $LOG
+check make no-user-vtk | tee -a $LOG
+
+echo "[STUBS]" | tee -a $LOG
+cd $BUILD_DIR/lammps-$LAMMPS_VERSION/src/STUBS
+make CC=icc | tee -a $LOG
+
+echo "[lib/awpmd]" | tee -a $LOG
+cd $BUILD_DIR/lammps-$LAMMPS_VERSION/lib/awpmd
+check make -j4 -f Makefile.mpicc | tee -a $LOG
+check echo "user-awpmd_SYSLIB = -mkl" >> Makefile.lammps
+echo "[lib/atc]" | tee -a $LOG
+cd $BUILD_DIR/lammps-$LAMMPS_VERSION/lib/atc
+check make -j4 -f Makefile.mpic++ | tee -a $LOG
+echo "[lib/colvars]" | tee -a $LOG
+cd $BUILD_DIR/lammps-$LAMMPS_VERSION/lib/colvars
+check make -j4 -f Makefile.g++ CXX=icpc | tee -a $LOG
+echo "[lib/h5md]" | tee -a $LOG
+cd $BUILD_DIR/lammps-$LAMMPS_VERSION/lib/h5md
+check make -j4 | tee -a $LOG
+check echo "h5md_SYSINC = -I$HDF5_ROOT/include" >> Makefile.lammps
+check echo "h5md_SYSLIB = -lhdf5" >> Makefile.lammps
+check echo "h5md_SYSPATH = -L$HDF5_ROOT/lib" >> Makefile.lammps
+echo "[lib/meam]" | tee -a $LOG
+cd $BUILD_DIR/lammps-$LAMMPS_VERSION/lib/meam
+check make -j4 -f Makefile.ifort | tee -a $LOG
+check echo "meam_SYSLIB = -lifcore -lsvml -liompstub5 -limf" >> Makefile.lammps
+check echo "meam_SYSPATH = -L$(dirname $(which ifort))/../../compiler/lib/intel64" >> Makefile.lammps
+echo "[lib/poems]" | tee -a $LOG
+cd $BUILD_DIR/lammps-$LAMMPS_VERSION/lib/poems
+check make -j4 -f Makefile.icc | tee -a $LOG
+echo "[lib/qmmm]" | tee -a $LOG
+cd $BUILD_DIR/lammps-$LAMMPS_VERSION/lib/qmmm
+check make -j4 -f Makefile.ifort | tee -a $LOG
+echo "[lib/reax]" | tee -a $LOG
+cd $BUILD_DIR/lammps-$LAMMPS_VERSION/lib/reax
+check make -j4 -f Makefile.ifort | tee -a $LOG
+echo "[lib/python]" | tee -a $LOG
+cd $BUILD_DIR/lammps-$LAMMPS_VERSION/lib/python
+check echo "python_SYSLIB = $(python-config --ldflags)" >> Makefile.lammps
+check echo "python_SYSPATH = -L$PYTHON_ROOT/lib" >> Makefile.lammps
+echo "[lib/smd]" | tee -a $LOG
+cd $BUILD_DIR/lammps-$LAMMPS_VERSION/lib/smd
+check echo "user-smd_SYSINC = -I$EIGEN3_ROOT/include/eigen3" >> Makefile.lammps
+
+echo "[make mpi]" | tee -a $LOG
+cd $BUILD_DIR/lammps-$LAMMPS_VERSION/src
+cp MAKE/OPTIONS/Makefile.icc_openmpi MAKE
+check make -j4 icc_openmpi | tee -a $LOG
+check make -j4 icc_openmpi mode=lib | tee -a $LOG
+check make -j4 icc_openmpi mode=shlib | tee -a $LOG
+
+echo "[lib/atc serial]" | tee -a $LOG
+cd $BUILD_DIR/lammps-$LAMMPS_VERSION/lib/atc
+check make -f Makefile.serial clean | tee -a $LOG
+check make -j4 -f Makefile.serial CC=icpc EXTRAMAKE=Makefile.lammps.installed | tee -a $LOG
+
+echo "[make serial]" | tee -a $LOG
+cd $BUILD_DIR/lammps-$LAMMPS_VERSION/src
+check make no-mpiio no-user-lb
+cp MAKE/OPTIONS/Makefile.icc_serial MAKE
+check make -j4 icc_serial | tee -a $LOG
+check make -j4 icc_serial mode=lib | tee -a $LOG
+check make -j4 icc_serial mode=shlib | tee -a $LOG
+
 echo "[make install]" | tee -a $LOG
+cd $BUILD_DIR/lammps-$LAMMPS_VERSION/src
 mkdir -p $PREFIX/bin $PREFIX/include $PREFIX/lib
 cp -p lmp_icc_serial $PREFIX/bin/lmp_serial
-cp -p lmp_mpi $PREFIX/bin
+cp -p lmp_icc_openmpi $PREFIX/bin/lmp_mpi
 cp -p lammps.h $PREFIX/include
 cp -p liblammps_* $PREFIX/lib
 cp -rp $BUILD_DIR/lammps-$LAMMPS_VERSION/examples $PREFIX
--- 75_lammps/setup.sh
+++ 75_lammps/setup.sh
@@ -7,11 +7,17 @@ set_prefix
 
 cd $BUILD_DIR
 if [ -d lammps-$LAMMPS_VERSION ]; then :; else
-  if [ -f $SOURCE_DIR/lammps-$LAMMPS_VERSION.tar.gz ]; then
-    check tar zxf $SOURCE_DIR/lammps_$LAMMPS_VERSION.tar.gz
-  else
-    check wget -O lammps-$LAMMPS_VERSION.tar.gz $WGET_OPTION https://github.com/lammps/lammps/archive/stable_${LAMMPS_VERSION}.tar.gz
-    check tar zxf lammps-$LAMMPS_VERSION.tar.gz
-    check mv lammps-stable_$LAMMPS_VERSION lammps-$LAMMPS_VERSION
+  if [ -f lammps-$LAMMPS_VERSION.tar.gz ]; then :; else
+    if [ -f $SOURCE_DIR/lammps-$LAMMPS_VERSION.tar.gz ]; then
+      cp $SOURCE_DIR/lammps_$LAMMPS_VERSION.tar.gz .
+    else
+      check wget -O lammps-$LAMMPS_VERSION.tar.gz $WGET_OPTION https://github.com/lammps/lammps/archive/stable_${LAMMPS_DOWNLOAD_VERSION}.tar.gz
+    fi
+  fi
+  check mkdir -p lammps-$LAMMPS_VERSION
+  check tar zxf lammps-$LAMMPS_VERSION.tar.gz -C lammps-$LAMMPS_VERSION --strip-components=1
+  if [ -f $SCRIPT_DIR/lammps-$LAMMPS_VERSION.patch ]; then
+    cd lammps-$LAMMPS_VERSION
+    patch -p1 < $SCRIPT_DIR/lammps-$LAMMPS_VERSION.patch
   fi
 fi
