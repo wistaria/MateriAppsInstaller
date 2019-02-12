2cc0cbee0158a264577cbcc84ed6536f6713fb37
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2019-02-12 16:38:22 +0900

    Fixed source directory

--- 00_wget/setup.sh
+++ 00_wget/setup.sh
@@ -8,24 +8,24 @@ set_prefix
 
 cd $BUILD_DIR
 if [ -d wget-$WGET_VERSION ]; then :; else
-  if [ -f $HOME/source/wget-$WGET_VERSION.tar.gz ]; then
-    check tar zxf $HOME/source/wget-$WGET_VERSION.tar.gz
+  if [ -f $SOURCE_DIR/wget-$WGET_VERSION.tar.gz ]; then
+    check tar zxf $SOURCE_DIR/wget-$WGET_VERSION.tar.gz
   else 
     check wget https://ftp.gnu.org/gnu/wget/wget-$WGET_VERSION.tar.gz
     check tar zxf wget-$WGET_VERSION.tar.gz
   fi
 fi
 if [ -d nettle-$NETTLE_VERSION ]; then :; else
-  if [ -f $HOME/source/nettle-$NETTLE_VERSION.tar.gz ]; then
-    check tar zxf $HOME/source/nettle-$NETTLE_VERSION.tar.gz
+  if [ -f $SOURCE_DIR/nettle-$NETTLE_VERSION.tar.gz ]; then
+    check tar zxf $SOURCE_DIR/nettle-$NETTLE_VERSION.tar.gz
   else 
     check wget https://ftp.gnu.org/gnu/nettle/nettle-$NETTLE_VERSION.tar.gz
     check tar zxf nettle-$NETTLE_VERSION.tar.gz
   fi
 fi
 if [ -d gnutls-$GNUTLS_VERSION ]; then :; else
-  if [ -f $HOME/source/gnutls-$GNUTLS_VERSION.tar.xz ]; then
-    check tar Jxf $HOME/source/gnutls-$GNUTLS_VERSION.tar.xz
+  if [ -f $SOURCE_DIR/gnutls-$GNUTLS_VERSION.tar.xz ]; then
+    check tar Jxf $SOURCE_DIR/gnutls-$GNUTLS_VERSION.tar.xz
   else 
     check wget https://ftp.gnu.org/gnu/gnutls/gnutls-$GNUTLS_VERSION.tar.xz
     check tar Jxf gnutls-$GNUTLS_VERSION.tar.xz
