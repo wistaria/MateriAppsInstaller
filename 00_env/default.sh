9da4967d626b242d7cba68f744a30a964f3e33b5
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2019-02-12 17:21:08 +0900

    Fix error message

--- 00_env/default.sh
+++ 00_env/default.sh
@@ -5,33 +5,17 @@ SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
 . $SCRIPT_DIR/version.sh
 set_prefix
 
+if [ -f $PREFIX_TOOL/env.sh ]; then
+  echo "Error: $PREFIX_TOOL/env.sh exists"
+  exit 127
+fi
+
 sh $SCRIPT_DIR/install_cm.sh
 
-cat << EOF > $BUILD_DIR/env-cxx03.sh
+cat << EOF > $BUILD_DIR/env.sh
 # env $(basename $0 .sh) $ENV_VERSION $ENV_MA_REVISION $(date +%Y%m%d-%H%M%S)
 export MA_ROOT_TOOL=$PREFIX_TOOL
 export MA_ROOT_APPS=$PREFIX_APPS
-export MA_CXX_STANDARD=cxx03
-export PATH=$PREFIX_TOOL/bin:\$PATH
-export LD_LIBRARY_PATH=$PREFIX_TOOL/lib:\$LD_LIBRARY_PATH
-unset i
-for i in $PREFIX_TOOL/env.d/*.sh ; do
-  if [ -r "\$i" ]; then
-    if [ "\${-#*i}" != "\$-" ]; then
-      . "\$i"
-    else
-      . "\$i" >/dev/null 2>&1
-    fi
-  fi
-done
-unset i
-EOF
-
-cat << EOF > $BUILD_DIR/env-cxx1y.sh
-# env $(basename $0 .sh) $ENV_VERSION $ENV_MA_REVISION $(date +%Y%m%d-%H%M%S)
-export MA_ROOT_TOOL=$PREFIX_TOOL
-export MA_ROOT_APPS=$PREFIX_APPS
-export MA_CXX_STANDARD=cxx1y
 export PATH=$PREFIX_TOOL/bin:\$PATH
 export LD_LIBRARY_PATH=$PREFIX_TOOL/lib:\$LD_LIBRARY_PATH
 unset i
@@ -48,7 +32,6 @@ unset i
 EOF
 
 mkdir -p $PREFIX_TOOL/env.d $PREFIX_TOOL/lib
-cp -f $BUILD_DIR/env-cxx03.sh $BUILD_DIR/env-cxx1y.sh $PREFIX_TOOL/
-rm -f $PREFIX_TOOL/env.sh
-ln -s env-cxx03.sh $PREFIX_TOOL/env.sh
-rm -f $BUILD_DIR/env-cxx03.sh $BUILD_DIR/env-cxx1y.sh
+rm -f $PREFIX_TOOL/env.sh $PREFIX_TOOL/env-cxx03.sh $PREFIX_TOOL/env-cxx1y.sh
+cp -f $BUILD_DIR/env.sh $PREFIX_TOOL/
+rm -f $BUILD_DIR/env.sh
