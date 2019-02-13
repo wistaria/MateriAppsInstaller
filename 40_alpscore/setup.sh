b35ca6f7e3f3378e87edd153f55b830a28c20066
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2019-02-13 10:03:05 +0900

    Removed cxx03 support

--- 40_alpscore/setup.sh
+++ 40_alpscore/setup.sh
@@ -5,17 +5,12 @@ SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
 . $SCRIPT_DIR/version.sh
 set_prefix
 
+sh $SCRIPT_DIR/download.sh
+
 cd $BUILD_DIR
 if [ -d alpscore-$ALPSCORE_VERSION ]; then :; else
-  if [ -f alpscore-$ALPSCORE_VERSION.tar.gz ]; then :; else
-    if [ -f $SOURCE_DIR/alpscore-$ALPSCORE_VERSION.tar.gz ]; then
-      cp $SOURCE_DIR/alpscore-$ALPSCORE_VERSION.tar.gz .
-    else
-      check wget -O alpscore-$ALPSCORE_VERSION.tar.gz https://github.com/ALPSCore/ALPSCore/archive/v$ALPSCORE_VERSION.tar.gz
-    fi
-  fi
   check mkdir -p alpscore-$ALPSCORE_VERSION
-  check tar zxf alpscore-$ALPSCORE_VERSION.tar.gz -C alpscore-$ALPSCORE_VERSION --strip-components=1
+  check tar zxf $SOURCE_DIR/alpscore-$ALPSCORE_VERSION.tar.gz -C alpscore-$ALPSCORE_VERSION --strip-components=1
   if [ -f $SCRIPT_DIR/alpscore-$ALPSCORE_VERSION.patch ]; then
     cd alpscore-$ALPSCORE_VERSION
     patch -p1 < $SCRIPT_DIR/alpscore-$ALPSCORE_VERSION.patch
