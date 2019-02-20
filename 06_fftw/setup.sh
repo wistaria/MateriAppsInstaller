9af06c9d06d5f9334e336930cd4bcade7c6ddfbd
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2019-02-20 12:09:10 +0900

    New upstream fftw 3.3.8

--- 06_fftw/setup.sh
+++ 06_fftw/setup.sh
@@ -2,16 +2,12 @@
 
 SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
 . $SCRIPT_DIR/../util.sh
-set_prefix
-. $PREFIX_TOOL/env.sh
 . $SCRIPT_DIR/version.sh
+set_prefix
+
+sh $SCRIPT_DIR/download.sh
 
 cd $BUILD_DIR
 if [ -d fftw-$FFTW_VERSION ]; then :; else
-  if [ -f $SOURCE_DIR/fftw-$FFTW_VERSION.tar.gz ]; then
-    check tar zxf $SOURCE_DIR/fftw-$FFTW_VERSION.tar.gz
-  else
-    check wget http://www.fftw.org/fftw-$FFTW_VERSION.tar.gz
-    check tar zxf fftw-$FFTW_VERSION.tar.gz
-  fi
+  check tar zxf $SOURCE_DIR/fftw-$FFTW_VERSION.tar.gz
 fi
