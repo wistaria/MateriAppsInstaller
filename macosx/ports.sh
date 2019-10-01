a79657ec868bfbaf2f5d40ea90e0d23d4db6da37
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2019-10-02 01:08:54 +0900

    Updated to gcc9 and python 3.7

--- macosx/ports.sh
+++ macosx/ports.sh
@@ -1,11 +1,11 @@
-port selfupdate         
+port selfupdate
 port -N install ld64 +ld64_xcode
-port -N install gcc8 openmpi-gcc8
-port select --set gcc mp-gcc8
-port select --set mpi openmpi-gcc8-fortran
-port -N install wget subversion cmake fftw-3 +gcc8 fftw-3-single +gcc8 gsl +gcc8
+port -N install gcc9 openmpi-gcc9
+port select --set gcc mp-gcc9
+port select --set mpi openmpi-gcc9-fortran
+port -N install wget subversion cmake fftw-3 +gcc9 fftw-3-single +gcc9 gsl +gcc9
 port -N install qt4-mac
-port -N install hdf5 +gcc8 +threadsafe
+port -N install hdf5 +gcc9 +threadsafe
 port -N install gnuplot
 
 port -N install python27 py27-pip py27-scipy py27-matplotlib
@@ -19,13 +19,13 @@ port select --set virtualenv virtualenv27
 port -N install py27-jupyter
 port select --set ipython py27-ipython
 port select --set ipython2 py27-ipython
-port -N install py27-wxpython-2.8 py27-opengl py27-mako py27-mpi4py +gcc8 +openmpi py27-h5py +gcc8 -openmpi
+port -N install py27-wxpython-2.8 py27-opengl py27-mako py27-mpi4py +gcc9 +openmpi py27-h5py +gcc9 -openmpi
 
-port -N install python36 py36-pip py36-scipy py36-matplotlib
-port select --set python3 python36
+port -N install python37 py37-pip py37-scipy py37-matplotlib
+port select --set python3 python37
 rm -f /opt/local/bin/pip3
 ln -s pip-3.6 /opt/local/bin/pip3
-port -N install py36-jupyter
-port select --set ipython3 py36-ipython
+port -N install py37-jupyter
+port select --set ipython3 py37-ipython
 rm -f /opt/local/bin/jupyter
 ln -s jupyter-3.6 /opt/local/bin/jupyter
