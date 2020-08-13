87a03c9b5dff13ee7a4ba04f39852e91d4ae0973
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2020-08-13 21:05:05 +0900

    dsqss

--- apps/dsqss/link.sh
+++ apps/dsqss/link.sh
@@ -1,21 +1,12 @@
 #!/bin/sh
 
 SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
-. $SCRIPT_DIR/../util.sh
+. $SCRIPT_DIR/../../scripts/util.sh
 . $SCRIPT_DIR/version.sh
 set_prefix
 
-. $PREFIX_TOOL/env.sh
+. $MA_ROOT/env.sh
 
-DSQSSVARS_SH=$PREFIX_APPS/dsqss/dsqssvars-$DSQSS_VERSION-$DSQSS_MA_REVISION.sh
-rm -f $PREFIX_APPS/dsqss/dsqssvars.sh
-ln -s $DSQSSVARS_SH $PREFIX_APPS/dsqss/dsqssvars.sh
-
-BUILD_ARCH="Linux-x86_64 Linux-s64fx"
-for arch in $BUILD_ARCH; do
-  DSQSSVARS_SH=$PREFIX_APPS/dsqss/dsqssvars-$arch-$DSQSS_VERSION-$DSQSS_MA_REVISION.sh
-  if [ -f $DSQSSVARS_SH ]; then
-    rm -f $PREFIX_APPS/dsqss/dsqssvars-$arch.sh
-    ln -s $DSQSSVARS_SH $PREFIX_APPS/dsqss/dsqssvars-$arch.sh
-  fi
-done
+VARS_SH=$MA_ROOT/${__NAME__}/${__NAME__}vars-$__VERSION__-$__MA_REVISION__.sh
+rm -f $MA_ROOT/${__NAME__}/${__NAME__}vars.sh
+ln -s $VARS_SH $MA_ROOT/${__NAME__}/${__NAME__}vars.sh
