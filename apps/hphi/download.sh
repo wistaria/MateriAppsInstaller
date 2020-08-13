84fa0464b0fd49644471fb11e62ff0d8ad543f4f
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2020-08-13 20:52:47 +0900

    fix a typo

--- apps/hphi/download.sh
+++ apps/hphi/download.sh
@@ -6,5 +6,5 @@ SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
 set_prefix
 
 if [ -f $SOURCE_DIR/${__NAME__}-${__VERSION__}.tar.gz ]; then :; else
-  _check wget https://github.com/issp-center-dev/hphi/releases/download/v${__VERSION__}/HPhi-${__VERSION__}.tar.gz -O $SOURCE_DIR/${__NAME__}-${__VERSION__}.tar.gz
+  check wget https://github.com/issp-center-dev/hphi/releases/download/v${__VERSION__}/HPhi-${__VERSION__}.tar.gz -O $SOURCE_DIR/${__NAME__}-${__VERSION__}.tar.gz
 fi
