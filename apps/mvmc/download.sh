9e993de783f4fd82d6a0469e5d9a6d43ec76e13e
Yuichi Motoyama <yomichi@tsg.jp>
2020-10-08 14:01:42 +0900

    update mVMC

--- apps/mvmc/download.sh
+++ apps/mvmc/download.sh
@@ -6,5 +6,5 @@ SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
 set_prefix
 
 if [ -f $SOURCE_DIR/${__NAME__}-${__VERSION__}.tar.gz ]; then :; else
-  _check wget https://github.com/issp-center-dev/mVMC/releases/download/v${__VERSION__}/mVMC-${__VERSION__}.tar.gz -O $SOURCE_DIR/${__NAME__}-${__VERSION__}.tar.gz
+  check wget https://github.com/issp-center-dev/mVMC/releases/download/v${__VERSION__}/mVMC-${__VERSION__}.tar.gz -O $SOURCE_DIR/${__NAME__}-${__VERSION__}.tar.gz
 fi
