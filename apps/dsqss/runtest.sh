585fbc0904387fd89e5eaae4e7a42d4987fc2db9
Yuichi Motoyama <yomichi@tsg.jp>
2020-10-21 11:56:03 +0900

    test for dsqss

---
+++ apps/dsqss/runtest.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+set -o pipefail
+
+# configurable variables (e.g. compiler)
+export OMP_NUM_THREADS=${OMP_NUM_THREADS:-1}
+export MPIEXEC=${MPIEXEC-"mpiexec"}
+export MPIEXEC_NPROCS_OPT=${MPIEXEC_NPROCS_OPT-"-np"}
+
+if [ -z "$MPIEXEC" ];then
+  export MPIEXEC_CMD=""
+else
+  export MPIEXEC_CMD="${MPIEXEC} ${MPIEXEC_NPROCS_OPT} 1"
+fi
+
+mode=${1:-default}
+SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
+
+. $SCRIPT_DIR/../../scripts/util.sh
+. $SCRIPT_DIR/version.sh
+set_prefix
+
+. ${MA_ROOT}/env.sh
+
+VARS_SH=${MA_ROOT}/${__NAME__}/${__NAME__}vars-${__VERSION__}-${__MA_REVISION__}.sh
+if [ ! -f "$VARS_SH" ]; then
+  echo "Error: configuration file (${VARS_SH}) does not exist"
+  exit 127
+fi
+echo "source $VARS_SH"
+. $VARS_SH
+
+ROOTNAME=$(toupper ${__NAME__})_ROOT
+eval rootdir='$'$ROOTNAME
+export rootdir
+if [ -z "$rootdir" ]; then
+  echo "Error: $ROOTNAME does not set in the configuration file (${VARS_SH})"
+  exit 127
+fi
+if [ ! -d "$rootdir" ]; then
+  echo "Error: $rootdir does not exist"
+  exit 127
+fi
+
+workdir="test_`date +%FT%T`"
+rm -rf $workdir
+cp -r test $workdir
+cd $workdir
+sh ./test.sh
+
+echo "Test finishes ($workdir)"
---
+++ apps/dsqss/test/test.sh
@@ -0,0 +1,19 @@
+for exe in dla pmwa_H pmwa_B; do
+  if [ ! -x ${rootdir}/bin/$exe ]; then
+    echo "Error: ${rootdir}/bin/${exe} does not exist"
+    exit 127
+  fi
+done
+
+cd dla
+dla_pre std.toml
+${MPIEXEC_CMD} dla param.in 2>&1 | tee -a ../log
+echo test for dla finished | tee -a ../log
+cd ..
+
+cd pmwa
+pmwa_pre std.in
+${MPIEXEC_CMD} pmwa_H param.in 2>&1 | tee -a ../log
+echo test for pmwa finished | tee -a ../log
+cd ..
+
