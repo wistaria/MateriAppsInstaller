c201d38cbf15fd68f6a8c8fc434ed7d25205cb51
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2020-11-06 22:17:22 +0900

    Add runtest to boost

---
+++ tools/boost/test/test.sh
@@ -0,0 +1,11 @@
+set -u
+set -o pipefail
+
+for exe in b2; do
+  if [ ! -e ${PREFIX}/bin/${exe} ]; then
+    echo "Error: ${PREFIX}/bin/${exe} does not exist"
+    exit 127
+  fi
+done
+
+cmake . || exit 127
