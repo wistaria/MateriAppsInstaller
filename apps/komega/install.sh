6a2c4844e61c65a804e9c4cb614a42d46bdf384e
Yuichi Motoyama <yomichi@tsg.jp>
2020-11-12 22:20:54 +0900

    komega

---
+++ apps/komega/install.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+set -o pipefail
+
+# configurable variables (e.g. compiler)
+export FC=${FC:-}
+export MA_EXTRA_FLAGS=${MA_EXTRA_FLAGS:-}
+export ISSP_UCOUNT=${ISSP_UCOUNT:-/home/issp/materiapps/bin/issp-ucount}
+
+mode=${1:-default}
+SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
+CONFIG_DIR=$SCRIPT_DIR/config/$mode
+if [ ! -d $CONFIG_DIR ]; then
+  echo "Error: unknown mode: $mode"
+  echo "Available list:"
+  ls -1 config
+  exit 127
+fi
+DEFAULT_CONFIG_DIR=$SCRIPT_DIR/config/default
+
+. $SCRIPT_DIR/../../scripts/util.sh
+. $SCRIPT_DIR/version.sh
+set_prefix
+
+. ${MA_ROOT}/env.sh
+export LOG=${BUILD_DIR}/${__NAME__}-${__VERSION__}-${__MA_REVISION__}.log
+export PREFIX="${MA_ROOT}/${__NAME__}/${__NAME__}-${__VERSION__}-${__MA_REVISION__}"
+
+if [ -d $PREFIX ]; then
+  echo "Error: $PREFIX exists"
+  exit 127
+fi
+
+sh ${SCRIPT_DIR}/setup.sh
+rm -rf $LOG
+cd ${BUILD_DIR}/${__NAME__}-${__VERSION__}
+start_info | tee -a $LOG
+
+for process in preprocess build install postprocess; do
+  echo "[${process}]" | tee -a $LOG
+  __file__=${process}.sh
+  if [ -f $CONFIG_DIR/$__file__ ]; then
+    check sh $CONFIG_DIR/$__file__
+  elif [ -f $DEFAULT_CONFIG_DIR/$__file__ ]; then
+    check sh $DEFAULT_CONFIG_DIR/$__file__
+  fi
+done
+
+finish_info | tee -a $LOG
+
+ROOTNAME=$(toupper ${__NAME__})_ROOT
+
+cat << EOF > ${BUILD_DIR}/${__NAME__}vars.sh
+# ${__NAME__} $(basename $0 .sh) ${__VERSION__} ${__MA_REVISION__} $(date +%Y%m%d-%H%M%S)
+. ${MA_ROOT}/env.sh
+export ${ROOTNAME}=$PREFIX
+export PATH=\${${ROOTNAME}}/bin:\$PATH
+EOF
+VARS_SH=${MA_ROOT}/${__NAME__}/${__NAME__}vars-${__VERSION__}-${__MA_REVISION__}.sh
+rm -f $VARS_SH
+cp -f ${BUILD_DIR}/${__NAME__}vars.sh $VARS_SH
+cp -f $LOG ${MA_ROOT}/${__NAME__}/
