0e248c5ec912fa33fdd611cb3dff090cd87632e2
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2020-11-18 13:54:34 +0900

    ci openmx

--- .github/scripts/openmx.sh
+++ .github/scripts/openmx.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd ${GITHUB_WORKSPACE}/apps/openmx
-LIB="-lscalapack-openmpi -llapack -lblas -lfftw3 -lmpi -lpthread -lm -lgfortran" sh install.sh
+LIB="-lscalapack-openmpi -llapack -lblas -lfftw3 -lpthread -lm -lgfortran" sh install.sh
 sh runtest.sh
 
