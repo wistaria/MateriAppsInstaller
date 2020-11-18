a4d12cf205f5b49a70ac4818c46cc75b5aa509d6
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2020-11-18 14:31:44 +0900

    update espresso/config/intel

--- apps/espresso/config/intel/preprocess.sh
+++ apps/espresso/config/intel/preprocess.sh
@@ -5,7 +5,7 @@ set -e
 CC=${CC:-icc}
 FC=${FC:-ifort}
 F90=${FC}
-MPIF90=${MPIF90:-mpiifort}
+MPIF90=${MPIF90:-mpif90}
 BLAS_LIBS=${BLAS_LIBS:-"-mkl=cluster"}
 
 CFLAGS="-O3 ${MA_EXTRA_FLAGS}" \
@@ -14,7 +14,7 @@ FFLAGS="-O3 ${MA_EXTRA_FLAGS}" \
   --prefix=${PREFIX} \
   --enable-openmp \
   --with-scalapack=yes \
-  CC=icc FC=ifort F77=ifort F90=ifort MPIF90=mpiifort
+  CC=${CC} FC=${FC} F77=${FC} F90=${F90} MPIF90=${MPIF90}
 
 sed -i.bak 's/^\s*F90\s*=.*$/F90 = ifort/' make.inc
 sed -i.bak "s/^\\s*BLAS_LIBS\\s*=.*$/BLAS_LIBS = ${BLAS_LIBS}/" make.inc
