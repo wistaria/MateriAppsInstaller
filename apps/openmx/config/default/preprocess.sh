c4bd61721cadbe36a6edb58b6d3140911636a629
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2020-11-18 13:05:24 +0900

    default value of LIB in openmx/config/default

--- apps/openmx/config/default/preprocess.sh
+++ apps/openmx/config/default/preprocess.sh
@@ -5,10 +5,12 @@ sh $(dirname $0)/../default/preprocess_common.sh
 CC=${CC:-mpicc}
 FC=${FC:-mpif90}
 OMP_FLAG=${OMP_FLAG:-"-fopenmp"}
+LIB=${LIB:-"-lscalapack -llapack -lblas -lfftw3 -lmpi -lpthread -lm -lgfortran"}
 
 echo CC=$CC
 echo FC=$FC
 echo OMP_FLAG=$OMP_FLAG
+echo LIB=$LIB
 
 cat << EOF > makefile_head
 CC = ${CC} ${MA_EXTRA_FLAGS} $OMP_FLAG -fcommon
