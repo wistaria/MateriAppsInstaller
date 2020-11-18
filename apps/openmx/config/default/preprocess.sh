4f3919ab6fd047357d4d5a0d639bf67b851bf1ce
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2020-11-18 12:22:11 +0900

    openmx

--- apps/openmx/config/default/preprocess.sh
+++ apps/openmx/config/default/preprocess.sh
@@ -4,13 +4,14 @@ sh $(dirname $0)/../default/preprocess_common.sh
 
 CC=${CC:-mpicc}
 FC=${FC:-mpif90}
+OMP_FLAG=${OMP_FLAG:-"-fopenmp"}
 
 echo CC=$CC
 echo FC=$FC
-echo OMP_FLAG=${OMP_FLAG:-"-fopenmp"}
+echo OMP_FLAG=$OMP_FLAG
 
 cat << EOF > makefile_head
-CC = ${CC} ${MA_EXTRA_FLAGS} $OMP_FLAG ${CFLAGS} -fcommon
+CC = ${CC} ${MA_EXTRA_FLAGS} $OMP_FLAG -fcommon
 FC = ${FC} ${MA_EXTRA_FLAGS} $OMP_FLAG -fcommon
 LIB = ${LIB}
 EOF
