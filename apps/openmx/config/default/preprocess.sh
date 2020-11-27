81786ab8b5edc569b64065659bc9926e5c4cfa39
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2020-11-28 01:38:04 +0900

    add CMakeLists.txt to OpenMX

--- apps/openmx/config/default/preprocess.sh
+++ apps/openmx/config/default/preprocess.sh
@@ -1,21 +1,8 @@
-cd source
+rm -rf build
+mkdir build
+cd build
 
-sh $(dirname $0)/../default/preprocess_common.sh
-
-CC=${CC:-mpicc}
-FC=${FC:-mpif90}
-OMP_FLAG=${OMP_FLAG:-"-fopenmp"}
-LIB=${LIB:-"-lscalapack -llapack -lblas -lfftw3 -lmpi -lpthread -lm -lgfortran"}
-
-echo CC=$CC
-echo FC=$FC
-echo OMP_FLAG=$OMP_FLAG
-echo LIB=$LIB
-
-cat << EOF > makefile_head
-CC = ${CC} ${MA_EXTRA_FLAGS} $OMP_FLAG -fcommon
-FC = ${FC} ${MA_EXTRA_FLAGS} $OMP_FLAG -fcommon
-LIB = ${LIB}
-EOF
-
-cat makefile_head makefile.org > makefile
+cmake \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DCMAKE_VERBOSE_MAKEFILE=1 \
+  ../
--- apps/openmx/config/default/preprocess_common.sh
+++
@@ -1,8 +0,0 @@
-if [ ! -f makefile.org ]; then
-  sed \
-  -e 's/^[ ]*\(CC[ ]*=\)/# \1/' \
-  -e 's/^[ ]*\(FC[ ]*=\)/# \1/' \
-  -e 's/^[ ]*\(LIB[ ]*=\)/# \1/' \
-  -e 's/^[ ]*\(MKLROOT[ ]*=\)/# \1/' \
-  makefile > makefile.org
-fi
