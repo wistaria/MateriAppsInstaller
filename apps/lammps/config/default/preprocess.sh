1c60c52300c5c6be92ae647fc37bfa61f6e37877
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2020-12-01 23:52:18 +0900

    lammps

---
+++ apps/lammps/config/default/preprocess.sh
@@ -0,0 +1,10 @@
+rm -rf build
+mkdir build
+cd build
+
+cmake -C../cmake/presets/all_on.cmake -C../cmake/presets/nolib.cmake \
+  -DBUILD_LIB=yes -DBUILD_SHARED_LIBS=yes \
+  -DPC_FFTW3_INCLUDE_DIRS=$FFTW_ROOT/include -DPC_FFTW3_LIBRARY_DIRS=$FFTW_ROOT/lib \
+  -DCMAKE_CXX_FLAGS="-DLMP_INTEL_NO_TBB" \
+  -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  ../cmake
