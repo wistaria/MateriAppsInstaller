7a0483e6c75f8355fae63749dd22c7a7d4841747
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2020-12-06 11:53:47 +0900

    Fix python2 script

--- tools/python2/config/macos/preprocess.sh
+++ tools/python2/config/macos/preprocess.sh
@@ -13,7 +13,7 @@ fi
 if [ -n "$TCLTK_ROOT" ]; then
   TCL_LIB=$(. ${TCLTK_ROOT}/lib/tclConfig.sh; echo ${TCL_LIB_FLAG})
   TK_LIB=$(. ${TCLTK_ROOT}/lib/tkConfig.sh; echo ${TK_LIB_FLAG})
-  ./configure CPPFLAGS="$CPPFLAGS" LDFLAGS="$LDFLAGS" --prefix=$PREFIX --enable-shared --enable-optimizations --with-system-ffi $OPENSSL_FLAGS --with-tcltk-includes="-I${TCLTK_ROOT}/include" --with-tcltk-libs="-L${TCLTK_ROOT}/lib $TCL_LIB $TK_LIB"
+  ./configure CPPFLAGS="$CPPFLAGS" LDFLAGS="$LDFLAGS" --prefix=$PREFIX --enable-shared --with-system-ffi $OPENSSL_FLAGS --with-tcltk-includes="-I${TCLTK_ROOT}/include" --with-tcltk-libs="-L${TCLTK_ROOT}/lib $TCL_LIB $TK_LIB"
 else
-  ./configure CPPFLAGS="$CPPFLAGS" LDFLAGS="$LDFLAGS" --prefix=$PREFIX --enable-shared --enable-optimizations --with-system-ffi $OPENSSL_FLAGS
+  ./configure CPPFLAGS="$CPPFLAGS" LDFLAGS="$LDFLAGS" --prefix=$PREFIX --enable-shared --with-system-ffi $OPENSSL_FLAGS
 fi
