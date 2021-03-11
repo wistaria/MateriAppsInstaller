be9acfe8b709dae0787a39c68c829747ba9cffb8
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2021-03-11 11:19:06 +0900

    hphi: fix for intel+openmpi

--- apps/hphi/config/fujitsu/preprocess.sh
+++ apps/hphi/config/fujitsu/preprocess.sh
@@ -1,11 +1,13 @@
+set -u
+
 rm -rf build
 mkdir build
 cd build
 
 ${CMAKE} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-  -DCMAKE_VERBOSE_MAKEFILE=1 \
   -DCMAKE_C_FLAGS="${MA_EXTRA_FLAGS}" \
   -DCMAKE_Fortran_FLAGS="${MA_EXTRA_FLAGS}" \
+  -DCMAKE_VERBOSE_MAKEFILE=1 \
   -DCONFIG=fujitsu \
   ../
--- apps/hphi/config/intel/preprocess.sh
+++ apps/hphi/config/intel/preprocess.sh
@@ -4,15 +4,30 @@ rm -rf build
 mkdir build
 cd build
 
+if [ -z ${CC+defined} ]; then
+  for m in mpicc mpiicc; do
+    CC=$(which $m 2> /dev/null)
+    test "$($CC --version 2> /dev/null | head -1 | cut -d ' ' -f 1)" = "icc" && break
+    CC=icc
+  done
+fi
+if [ -z ${FC+defined} ]; then
+  for m in mpifort mpiifort; do
+    FC=$(which $m 2> /dev/null)
+    test "$($FC --version 2> /dev/null | head -1 | cut -d ' ' -f 1)" = "ifort" && break
+    FC=ifort
+  done
+fi
+export CC
+export FC
+
 ${CMAKE} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-  -DCMAKE_VERBOSE_MAKEFILE=1 \
-  -DCMAKE_C_COMPILER=`which icc` \
-  -DCMAKE_Fortran_COMPILER=`which ifort` \
-  -DCMAKE_C_FLAGS="${MA_EXTRA_FLAGS}" \
   -DCMAKE_C_FLAGS_RELEASE="-O3 -DNDEBUG -DHAVE_SSE2" \
-  -DCMAKE_Fortran_FLAGS="${MA_EXTRA_FLAGS}" \
+  -DCMAKE_C_FLAGS="${MA_EXTRA_FLAGS}" \
   -DCMAKE_Fortran_FLAGS_RELEASE="-O3 -DNDEBUG -DHAVE_SSE2" \
+  -DCMAKE_Fortran_FLAGS="${MA_EXTRA_FLAGS}" \
+  -DCMAKE_VERBOSE_MAKEFILE=1 \
   -DUSE_SCALAPACK=ON \
   -DSCALAPACK_LIBRARIES="-mkl=cluster" \
   ../
