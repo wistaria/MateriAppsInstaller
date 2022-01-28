bc6d06a0a00e65ea0094070b3093846e27a95f91
Yuichi Motoyama <yomichi@tsg.jp>
2022-01-28 12:35:46 +0900

    subdomain of the pseudo potential server is changed

--- apps/espresso/test/download_pp.sh
+++ apps/espresso/test/download_pp.sh
@@ -2,7 +2,7 @@ ppfile=Si.pbe-n-kjpaw_psl.1.0.0.UPF
 pppath=${TESTDIR}/${ppfile}
 if [ ! -f $pppath ] ; then
   echo "Start downloading pseudo potential file"
-  wget https://www.quantum-espresso.org/upf_files/${ppfile} -O $pppath
+  wget https://pseudopotentials.quantum-espresso.org/upf_files/${ppfile} -O $pppath
   if [ $? != 0 ]; then
     echo "Failed to download the pseudo potential file"
     echo "Download ${ppfile} manually from QE official site"
