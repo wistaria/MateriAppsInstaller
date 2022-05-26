8e2edb1fbfad90f28452176275cfe1164401b9f7
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2022-05-26 09:21:20 +0900

    fix #122 for homebrew on intel mac

--- tools/gcc-wrapper/install.sh
+++ tools/gcc-wrapper/install.sh
@@ -13,6 +13,14 @@ if [ -d ${PREFIX} ]; then
   exit 127
 fi
 
+if [ -z ${HOMEBREW_PREFIX} ]; then
+  if [ -e "/opt/homebrew/bin/brew" ]; then
+    eval $(/opt/homebrew/bin/brew shellenv)
+  elif [ -e "/usr/local/bin/brew" ]; then
+    eval $(/usr/local/bin/brew shellenv)
+  fi
+fi
+
 FOUND=0
 if [ -n ${HOMEBREW_PREFIX} ]; then
   VERSIONS="15 14 13 12 11 10 9 8"
