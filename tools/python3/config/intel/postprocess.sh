b6c35b0fdbcdac583442024c86822b60f6af3298
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2022-09-23 19:39:03 +0900

    [python] update to 3.9.14 / use intel-numpy, intel-scipy

--- tools/python3/config/intel/postprocess.sh
+++ tools/python3/config/intel/postprocess.sh
@@ -18,23 +18,11 @@ check ${PIP} install --upgrade pip
 echo "[setuptools]"
 check ${PIP} install setuptools wheel
 
-rm -f numpy-site.cfg.org
-if [ -e ~/numpy-site.cfg ]; then
-  cp ~/numpy-site.cfg ./numpy-site.cfg.org
-fi
-cat << EOF > ~/numpy-site.cfg
-[mkl]
-library_dirs = $(echo $MKLROOT | cut -d: -f1)/lib/intel64
-include_dirs = $(echo $MKLROOT | cut -d: -f1)/include
-mkl_libs = mkl_rt
-lapack_libs = 
-EOF
-
 echo "[numpy]"
-check ${PIP} install --install-option="config" --install-option="--compiler=intelem build_clib" --install-option="--compiler=intelem build_ext" --install-option="--compiler=intelem" numpy
+check ${PIP} install intel-numpy
 
 echo "[scipy]"
-check ${PIP} install --install-option="config" --install-option="--compiler=intelem" --install-option="--fcompiler=intelem build_clib" --install-option="--compiler=intelem" --install-option="--fcompiler=intelem build_ext" --install-option="--compiler=intelem" --install-option="--fcompiler=intelem" scipy
+check ${PIP} install intel-scipy
 
 echo "[matplotlib]"
 check ${PIP} install matplotlib
