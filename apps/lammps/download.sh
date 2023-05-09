0b608209d1e3b8b2d35ed1adcf731f21911f5703
Yuichi Motoyama <yomichi@tsg.jp>
2023-05-09 11:27:51 +0900

    Lammps 20220623

--- apps/lammps/download.sh
+++ apps/lammps/download.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
 . $SCRIPT_DIR/version.sh
 set_prefix
 
-URL=https://github.com/lammps/lammps/archive/${LAMMPS_DOWNLOAD_VERSION}.tar.gz
+URL=https://github.com/lammps/lammps/archive/refs/tags/${LAMMPS_DOWNLOAD_VERSION}.tar.gz
 ARCHIVE=${SOURCE_DIR}/${__NAME__}-${__VERSION__}.tar.gz
 
 if [ -f $ARCHIVE ]; then :; else
