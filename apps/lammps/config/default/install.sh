6142a5e65023bffc234650ca4fbb32da00179750
Yuichi Motoyama <yomichi@tsg.jp>
2023-05-09 12:22:44 +0900

    make symlink

--- apps/lammps/config/default/install.sh
+++ apps/lammps/config/default/install.sh
@@ -1,3 +1,6 @@
 cd build
 make ${MAKE_J} install
+if [ ! -e $PREFIX/bin/lammps ]; then
+  ln -s $PREFIX/bin/lmp $PREFIX/bin/lammps
+fi
 cp -rp $BUILD_DIR/lammps-${__VERSION__}/examples $PREFIX/share/lammps/ || true
