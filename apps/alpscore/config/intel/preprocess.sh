6940627c3b8719b4c5cfdb5d81cf96c2bab75730
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2023-06-29 13:07:53 +0900

    [alpscore] support Intel oneAPI

--- apps/alpscore/config/intel/preprocess.sh
+++ apps/alpscore/config/intel/preprocess.sh
@@ -7,16 +7,18 @@ cd build
 if [ -z ${CC+defined} ]; then
   for m in mpicc mpiicc; do
     CC=$(which $m 2> /dev/null)
+    test "$($CC --version 2> /dev/null | head -1 | cut -d ' ' -f 2)" = "oneAPI" && break
     test "$($CC --version 2> /dev/null | head -1 | cut -d ' ' -f 1)" = "icc" && 
 break
-    CC=icc
+    CC=$(which icx || which icc)
   done
 fi
 if [ -z ${CXX+defined} ]; then
   for m in mpicxx mpic++ mpiicpc; do
     CXX=$(which $m 2> /dev/null)
+    test "$($CXX --version 2> /dev/null | head -1 | cut -d ' ' -f 2)" = "oneAPI" && break
     test "$($CXX --version 2> /dev/null | head -1 | cut -d ' ' -f 1)" = "icpc" && break
-    CXX=icpc
+    CXX=$(which icpx || which icpc)
   done
 fi
 export CC
