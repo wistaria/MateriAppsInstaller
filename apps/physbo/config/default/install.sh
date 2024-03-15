d656de90456c119803b32cd12e21a05990e0a4ea
Yuichi Motoyama <yomichi@tsg.jp>
2024-03-15 16:33:56 +0900

    make pip install compatible with Ubuntu 22.04

--- apps/physbo/config/default/install.sh
+++ apps/physbo/config/default/install.sh
@@ -1,2 +1,7 @@
 python3 -m pip install --prefix=$PREFIX ./
+
+if [ -d ${PREFIX}/local ]; then
+  PREFIX=${PREFIX}/local
+fi
+
 cp -r examples $PREFIX
--- apps/py2dmat/config/default/install.sh
+++ apps/py2dmat/config/default/install.sh
@@ -1,3 +1,8 @@
 python3 -m pip install --prefix=$PREFIX ./
+
+if [ -d ${PREFIX}/local ]; then
+  PREFIX=${PREFIX}/local
+fi
+
 cp -r sample $PREFIX
 cp -r script $PREFIX
