f02189b3218498d68d6e4d327d7f110c9386b9e9
Yuichi Motoyama <yomichi@tsg.jp>
2024-03-22 14:13:54 +0900

    update

--- apps/komega/config/default/preprocess.sh
+++ apps/komega/config/default/preprocess.sh
@@ -3,14 +3,18 @@ set -u
 FC=${FC:-mpifort}
 export FC
 
-check_file=check_compile_option.f90
+check_file_exe=check_file_option
+check_file=${check_file_exe}.f90
 cat << EOF > $check_file
 program check_compile_option
 end
 EOF
 
-${FC} -fallow-argument-mismatch $check_file >/dev/null 2>/dev/null
-if [ $? -eq 0 ]; then
+${FC} -o $check_file_exe -fallow-argument-mismatch $check_file >/dev/null 2>/dev/null
+st=$?
+rm -f $check_file
+rm -f $check_file_exe
+if [ $st -eq 0 ]; then
   ./configure --prefix=$PREFIX --with-mpi FCFLAGS="-fallow-argument-mismatch"
 else
   ./configure --prefix=$PREFIX --with-mpi
